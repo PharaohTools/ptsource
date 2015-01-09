39366448368ac9379d1d558b80681fb5cde06829
David Amanshia <phpengine@hotmail.co.uk>
2015-01-09 19:46:35 +0000

    added getDAta method

--- src/Modules/BuildHome/Model/BuildHomeAllOS.php
+++ src/Modules/BuildHome/Model/BuildHomeAllOS.php
@@ -14,36 +14,16 @@ class BuildHomeAllOS extends Base {
     // Model Group
     public $modelGroup = array("Default") ;
 
-    public function findModuleNames($params) {
+
+
+
+
+    public function getData($params) {
         if (isset($this->params["compatible-only"]) && $this->params["compatible-only"]=="true") {
             return $this->findOnlyCompatibleModuleNames($params); }
         if (isset($this->params["only-compatible"]) && $this->params["only-compatible"]=="true") {
             return $this->findOnlyCompatibleModuleNames($params); }
         return $this->findAllModuleNames() ;
-    }
-
-    private function findAllModuleNames() {
-        $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
-        $moduleNames = array() ;
-        foreach ($allInfoObjects as $infoObject) {
-            $array_keys = array_keys($infoObject->routesAvailable()) ;
-            $miniRay = array() ;
-            $miniRay["command"] = $array_keys[0] ;
-            $miniRay["name"] = $infoObject->name ;
-            $miniRay["hidden"] = $infoObject->hidden ;
-            $moduleNames[] = $miniRay ; }
-        return $moduleNames;
-    }
-
-    private function findOnlyCompatibleModuleNames($params) {
-        $allModules = $this->findAllModuleNames() ;
-        $controllerBase = new \Controller\Base();
-        $errors = $controllerBase->checkForRegisteredModels($params, $allModules) ;
-        $compatibleModules = array();
-        foreach($allModules as $oneModule) {
-            if (!in_array($oneModule["command"], $errors)) {
-                $compatibleModules[] = $oneModule ; } }
-        return $compatibleModules;
     }
 
 }
