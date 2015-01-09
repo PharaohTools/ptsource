9a62add88f52497e442ee99459a081b6c8a62ca2
David Amanshia <phpengine@hotmail.co.uk>
2015-01-09 19:43:45 +0000

    minor sytax updates for the build Home page of phrank

--- src/Modules/BuildHome/Views/BuildHomeHTMLView.tpl.php
+++ src/Modules/BuildHome/Views/BuildHomeHTMLView.tpl.php
@@ -8,19 +8,19 @@
             </div>
             <div class="list-group sidebar-list">
                         <span href="#" class="list-group-item active">
-                            Submenu
+                            Menu
                             <span class="pull-right" id="slide-submenu">
                                 <i class="fa fa-times"></i>
                             </span>
                         </span>
                 <a href="#" class="list-group-item">
-                    <i class="fa fa-comment-o"></i> Lorem ipsum
+                    <i class="fa fa-comment-o"></i> New Pipeline
                 </a>
                 <a href="#" class="list-group-item">
-                    <i class="fa fa-search"></i> Lorem ipsum
+                    <i class="fa fa-search">Configure Phrankinsense</i>
                 </a>
                 <a href="#" class="list-group-item">
-                    <i class="fa fa-user"></i> Lorem ipsum
+                    <i class="fa fa-user">List Pipelines</i>
                 </a>
                 <a href="#" class="list-group-item">
                     <i class="fa fa-folder-open-o"></i> Lorem ipsum <span class="badge">14</span>
@@ -37,9 +37,9 @@
         <div class="col-sm-8 col-md-9 clearfix main-container">
             <h4 class="text-uppercase text-light">GC cleopetra</h4>
             <div class="row clearfix no-margin">
-                <h3><a class="lg-anchor text-light" href="">Phakanese Pharao tools <i style="font-size: 18px;" class="fa fa-chevron-right"></i></a></h3>
+                <h3><a class="lg-anchor text-light" href="">Phrankinsense - Pharaoh Tools <i style="font-size: 18px;" class="fa fa-chevron-right"></i></a></h3>
                 <p>
-                    Build and Monitoring Server  in PHP.
+                    The Build and Monitoring Server built for the Web, in its favourite language.
                     <br/>
                     The home page for a single build
                 </p>
