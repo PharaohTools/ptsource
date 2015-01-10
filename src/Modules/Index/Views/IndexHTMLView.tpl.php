ebaccd030d53ada06b3096cc25c9cc44687e14b6
David Amanshia <phpengine@hotmail.co.uk>
2015-01-10 19:48:40 +0000

    demo pipelines in file, index menu formatting

--- src/Modules/Index/Views/IndexHTMLView.tpl.php
+++ src/Modules/Index/Views/IndexHTMLView.tpl.php
@@ -13,14 +13,14 @@
                                 <i class="fa fa-times"></i>
                             </span>
                         </span>
-                <a href="/index.php?control=BulidConfigure&action=new" class="list-group-item">
-                    <i class="fa fa-comment-o"></i> New Pipeline
+                <a href="/index.php?control=Index&action=show" class="list-group-item">
+                    <i class="fa fa-comment-o"></i> Dashboard
                 </a>
                 <a href="/index.php?control=ApplicationConfigure&action=show" class="list-group-item">
                     <i class="fa fa-search"></i> Configure Phrankinsense
                 </a>
                 <a href="/index.php?control=BuildList&action=show" class="list-group-item">
-                    <i class="fa fa-search"></i>All Pipelines
+                    <i class="fa fa-search"></i> All Pipelines
                 </a>
                 <a href="#" class="list-group-item">
                     <i class="fa fa-folder-open-o"></i> Lorem ipsum <span class="badge">14</span>
---
+++ src/Modules/Pipeline/Model/demopipelines.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: golden
+ * Date: 10/01/15
+ * Time: 19:31
+ * To change this template use File | Settings | File Templates.
+ */
