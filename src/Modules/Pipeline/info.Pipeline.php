f958b41684d1b611beba3f32aeab498ff37c773e
David Amanshia <phpengine@hotmail.co.uk>
2015-01-10 03:35:56 +0000

    added first draft of pipeline functionality module

---
+++ src/Modules/Pipeline/info.Pipeline.php
@@ -0,0 +1,30 @@
+<?php
+
+Namespace Info;
+
+class BuildConfigureInfo extends CleopatraBase {
+
+    public $hidden = false;
+
+    public $name = "BuildConfigure/Home Page";
+
+    public function _construct() {
+      parent::__construct();
+    }
+
+    public function routesAvailable() {
+      return array( "BuildConfigure" => array("show") );
+    }
+
+    public function routeAliases() {
+      return array("buildConfigure"=>"BuildConfigure");
+    }
+
+    public function helpDefinition() {
+      $help = <<<"HELPDATA"
+  This command is part of Core - its the default route and only used for help and as an Intro really...
+HELPDATA;
+      return $help ;
+    }
+
+}
