f9653d6f2e5902a71fc37e71140f36249fe2aba6
David Amanshia <phpengine@hotmail.co.uk>
2015-01-10 19:55:51 +0000

    build configure updated to get data for a build

--- src/Modules/BuildConfigure/Model/BuildConfigureAllOS.php
+++ src/Modules/BuildConfigure/Model/BuildConfigureAllOS.php
@@ -14,36 +14,15 @@ class BuildConfigureAllOS extends Base {
     // Model Group
     public $modelGroup = array("Default") ;
 
-    public function findModuleNames($params) {
-        if (isset($this->params["compatible-only"]) && $this->params["compatible-only"]=="true") {
-            return $this->findOnlyCompatibleModuleNames($params); }
-        if (isset($this->params["only-compatible"]) && $this->params["only-compatible"]=="true") {
-            return $this->findOnlyCompatibleModuleNames($params); }
-        return $this->findAllModuleNames() ;
+    public function getData() {
+        $ret["pipeline"] = $this->getPipeline();
+        return $ret ;
     }
 
-    private function findAllModuleNames() {
-        $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
-        $moduleNames = array() ;
-        foreach ($allInfoObjects as $infoObject) {
-            $array_keys = array_keys($infoObject->routesAvailable()) ;
-            $miniRay = array() ;
-            $miniRay["command"] = $array_keys[0] ;
-            $miniRay["name"] = $infoObject->name ;
-            $miniRay["hidden"] = $infoObject->hidden ;
-            $moduleNames[] = $miniRay ; }
-        return $moduleNames;
-    }
-
-    private function findOnlyCompatibleModuleNames($params) {
-        $allModules = $this->findAllModuleNames() ;
-        $controllerBase = new \Controller\Base();
-        $errors = $controllerBase->checkForRegisteredModels($params, $allModules) ;
-        $compatibleModules = array();
-        foreach($allModules as $oneModule) {
-            if (!in_array($oneModule["command"], $errors)) {
-                $compatibleModules[] = $oneModule ; } }
-        return $compatibleModules;
+    public function getPipeline() {
+        $pipelineFactory = new \Model\Pipeline() ;
+        $pipeline = $pipelineFactory->getModel($this->params);
+        return $pipeline->getPipeline($this->params["item"]);
     }
 
 }
