f729c8f7133b2ad4c2d6deacf217a03162ad6860
David Amanshia <phpengine@hotmail.co.uk>
2015-01-11 06:34:42 +0000

    piperunner handling services

--- src/Modules/PipeRunner/Controller/PipeRunner.php
+++ src/Modules/PipeRunner/Controller/PipeRunner.php
@@ -9,6 +9,10 @@ class PipeRunner extends Base {
         // if we don't have an object, its an array of errors
         $this->content = $pageVars ;
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        if ($pageVars["route"]["action"] == "service") {
+            $this->content["data"] = $thisModel->getServiceData();
+            $this->content["route"]["extraParams"]["output-format"] = "service";
+            return array ("type"=>"view", "view"=>"pipeRunner", "pageVars"=>$this->content); }
         $this->content["data"] = $thisModel->getData();
         return array ("type"=>"view", "view"=>"pipeRunner", "pageVars"=>$this->content);
     }
