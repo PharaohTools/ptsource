3e7b7b8e1a42c14bfc86d48c91525195342b8f1a
David Amanshia <phpengine@hotmail.co.uk>
2015-01-11 04:31:40 +0000

    returning correctly formed app configs

--- src/Modules/ApplicationConfigure/Model/ApplicationConfigureAllOS.php
+++ src/Modules/ApplicationConfigure/Model/ApplicationConfigureAllOS.php
@@ -14,36 +14,48 @@ class ApplicationConfigureAllOS extends Base {
     // Model Group
     public $modelGroup = array("Default") ;
 
-    public function findModuleNames($params) {
-        if (isset($this->params["compatible-only"]) && $this->params["compatible-only"]=="true") {
-            return $this->findOnlyCompatibleModuleNames($params); }
-        if (isset($this->params["only-compatible"]) && $this->params["only-compatible"]=="true") {
-            return $this->findOnlyCompatibleModuleNames($params); }
-        return $this->findAllModuleNames() ;
+    public function getData() {
+        $ret["app_configs"] = $this->getAppConfigs();
+        $ret["mod_configs"] = $this->getModuleConfigs();
+        return $ret ;
     }
 
-    private function findAllModuleNames() {
-        $allInfoObjects = \Core\AutoLoader::getInfoObjects() ;
-        $moduleNames = array() ;
-        foreach ($allInfoObjects as $infoObject) {
-            $array_keys = array_keys($infoObject->routesAvailable()) ;
-            $miniRay = array() ;
-            $miniRay["command"] = $array_keys[0] ;
-            $miniRay["name"] = $infoObject->name ;
-            $miniRay["hidden"] = $infoObject->hidden ;
-            $moduleNames[] = $miniRay ; }
-        return $moduleNames;
+    private function getAppConfigs() {
+        $configs = array(
+                    "instance_id" => array(
+                        "label" => "Instance ID",
+                        "type" => "text",
+                        "desc" => "Enter an instance ID that will generated",
+                        "default" => "1234567890", ),
+                    "instance_title" => array(
+                        "label" => "Instance Title",
+                        "type" => "text",
+                        "desc" => "Enter the title you'd like to apply to this instance (for clustering)",
+                        "default" => "My Development Instance" ),
+                    "organisation" => array(
+                        "label" => "Instance Title",
+                        "type" => "text",
+                        "desc" => "Enter the name of your Organisation",
+                        "default" => "An Organisation" ),
+                    "force_ssl" => array(
+                        "label" => "Force SSL",
+                        "type" => "boolean",
+                        "desc" => "Switch on to force web connections to be made via SSL",
+                        "default" => false ),
+                    );
+        return $configs;
     }
 
-    private function findOnlyCompatibleModuleNames($params) {
-        $allModules = $this->findAllModuleNames() ;
-        $controllerBase = new \Controller\Base();
-        $errors = $controllerBase->checkForRegisteredModels($params, $allModules) ;
-        $compatibleModules = array();
-        foreach($allModules as $oneModule) {
-            if (!in_array($oneModule["command"], $errors)) {
-                $compatibleModules[] = $oneModule ; } }
-        return $compatibleModules;
+    private function getModuleConfigs() {
+        $configs = array();
+        $infos = \Core\AutoLoader::getInfoObjects();
+        foreach ($infos as $info) {
+            if (method_exists($info, "configuration")) {
+                $fullName = get_class($info) ;
+                $modName = str_replace("Info", "", $fullName) ;
+                $modName = str_replace("\\", "", $modName) ;
+                $configs[$modName] = $info->configuration() ; } }
+        return $configs ;
     }
 
 }
