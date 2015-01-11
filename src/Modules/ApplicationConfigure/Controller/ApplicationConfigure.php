8282e28eeb456c12ce44a576e811baa427db3326
David Amanshia <phpengine@hotmail.co.uk>
2015-01-11 04:32:17 +0000

    application configiure usoing getdata

--- src/Modules/ApplicationConfigure/Controller/ApplicationConfigure.php
+++ src/Modules/ApplicationConfigure/Controller/ApplicationConfigure.php
@@ -5,12 +5,12 @@ Namespace Controller ;
 class ApplicationConfigure extends Base {
 
     public function execute($pageVars) {
-      $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
-      // if we don't have an object, its an array of errors
-      $this->content = $pageVars ;
-      if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-      $this->content["modulesInfo"] = $thisModel->findModuleNames($pageVars["route"]["extraParams"]);
-      return array ("type"=>"view", "view"=>"applicationConfigure", "pageVars"=>$this->content);
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        $this->content = $pageVars ;
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        $this->content["data"] = $thisModel->getData();
+        return array ("type"=>"view", "view"=>"applicationConfigure", "pageVars"=>$this->content);
     }
 
 }
