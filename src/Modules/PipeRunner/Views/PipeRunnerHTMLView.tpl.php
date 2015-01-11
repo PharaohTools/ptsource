8d8de288a448835c7e80aa3e97def3e13f1ace5f
David Amanshia <phpengine@hotmail.co.uk>
2015-01-11 22:26:33 +0000

    using local jquery incase we're offline

--- src/Modules/PipeRunner/Views/PipeRunnerHTMLView.tpl.php
+++ src/Modules/PipeRunner/Views/PipeRunnerHTMLView.tpl.php
@@ -46,24 +46,41 @@
                 </h5>
                 <form class="form-horizontal custom-form">
 
+                    <?php
+                    if (isset($pageVars["pipex"])) {
+                        ?>
+
+                        <div class="form-group">
+                            <div class="col-sm-10">
+                                Pipeline Execution started - PID <?= $pageVars["pipex"] ;?>
+                            </div>
+                        </div>
+
+                    <?php
+                    }
+                    ?>
+
                     <div class="form-group">
                         <div class="col-sm-10">
                             <div id="updatable">
-                                Starting Pipeline Execution...
+                                Checking Pipeline Execution Output...
                             </div>
                         </div>
                     </div>
 
+                    <script src="/Assets/js/jquery.min.js" type="text/javascript" />
+
                     <script type="text/javascript">
-                        console.log("one") ;
                         done = false ;
                         max = 0 ;
                         while (done == false && max < 30) {
-                            window.setTimeout(updatePage, 3000) //wait 3 seconds before continuing
+                            window.setTimeout(updatePage, 1000) //wait 3 seconds before continuing
                             max = max + 1 ; }
 
-                        function updatePage()
-                        {
+                        // updatePage() ;
+
+                        function updatePage() {
+                            console.log("running update page js method");
                             item = $("#item").val();
                             url = "/index.php?control=PipeRunner&action=service&item=" + item ;
                             $.ajax({
@@ -72,33 +89,65 @@
                                     $('#updatable').html(data); },
                                 complete: function() {
                                     // Schedule the next request when the current one's complete
-                                    setTimeout(updatePage, 5000); }
-                            });}
+                                    setStatus();
+                                    console.log(window.reqStatus);
+                                    if (window.reqStatus == "OK") {
+                                        doCompletion();
+                                    } else {
+                                        setTimeout(updatePage, 3000); } }
+                            });
+                        }
+
+                        function setStatus() {
+                            item = $("#item").val();
+                            pid = $("#pid").val();
+                            url = "/index.php?control=PipeRunner&action=pipestatus&item=" + item + "&pid=" + pid ;
+                            console.log(url);
+                            $.ajax({
+                                url: url,
+                                success: function(data) {
+                                    console.log(data);
+                                    window.reqStatus = data }
+                            });
+                        }
+
+                        function doCompletion() {
+                            removeWaitImage();
+                            changeSubButton();
+                        }
+
+                        function removeWaitImage() {
+                            $("#loading-holder").hide() ;
+                        }
+
+                        function changeSubButton() {
+                            subhtml  = '<div class="col-sm-offset-2 col-sm-8">';
+                            subhtml += '  <div class="text-center">';
+                            subhtml += '    <button type="submit" class="btn btn-primary" id="close-complete">Close Execution Screen</button>';
+                            subhtml += '  </div>';
+                            subhtml += '</div>' ;
+                            $("#submit-holder").html(subhtml) ;
+                        }
+
                     </script>
 
-                    <?php
-                        foreach ($pageVars["data"]["pipeline"]["steps"] as $one_build_step) {
-                            echo '<div class="form-group">' ;
-                            echo '  <label for="inputPassword3" class="col-sm-2 control-label text-left">'.$one_build_step["title"].'</label>' ;
-                            echo '  <div class="col-sm-10">' ;
-                            echo '      <p>'.$one_build_step["title"].'</p>';
-                            echo '      <p>Step Type: '.$one_build_step["type"]."</p>";
-                            echo '      <textarea class="form-control">'.$one_build_step["value"].'</textarea>';
-                            echo '  </div>';
-                            echo '</div>'; }
-                    ?>
-                    <div class="form-group">
+                    <div class="form-group" id="loading-holder">
+                        <div class="col-sm-offset-2 col-sm-8">
+                            <div class="text-center">
+                                <img class="loadingImage" src="/Assets/PipeRunner/images/loading.gif" />'
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group" id="submit-holder">
                         <div class="col-sm-offset-2 col-sm-8">
                             <div class="text-center">
                                 <button type="submit" class="btn btn-danger" id="end-now">End Now</button>
                             </div>
                         </div>
-                        <div class="col-sm-offset-2 col-sm-8">
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary" id="close-complete">Close Execution Screen</button>
-                            </div>
-                        </div>
                     </div>
+                    <input type="hidden" id="item" value="<?= $pageVars["data"]["pipeline"]["project_slug"]["value"] ;?>" />
+                    <input type="hidden" id="pid" value="<?= $pageVars["pipex"] ;?>" />
+
                 </form>
             </div>
             <p>
