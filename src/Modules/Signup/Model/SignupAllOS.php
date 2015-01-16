89093bb841f772138067876106fe96ec3ca3728b
Reazul Hoque <riyad.cse@workspaceit.com>
2015-01-16 13:59:34 +0600

    add Model files for Signup Modules

---
+++ src/Modules/Signup/Model/SignupAllOS.php
@@ -0,0 +1,22 @@
+<?php
+
+Namespace Model;
+
+class SignupAllOS extends Base {
+
+    // Compatibility
+    public $os = array("any") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("Default") ;
+
+    public function getloginV() {
+        $ret="get Login";
+        return $ret ;
+    }
+
+}
