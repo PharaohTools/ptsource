490ae5440682ca34025c3fb221d88a7cc7cfa6f9
David Amanshia <phpengine@hotmail.co.uk>
2015-01-17 15:44:23 +0000

    build config model; handles saves

--- src/Modules/BuildConfigure/Model/BuildConfigureAllOS.php
+++ src/Modules/BuildConfigure/Model/BuildConfigureAllOS.php
@@ -19,10 +19,24 @@ class BuildConfigureAllOS extends Base {
         return $ret ;
     }
 
+    public function saveState() {
+        return $this->savePipeline();
+    }
+
     public function getPipeline() {
         $pipelineFactory = new \Model\Pipeline() ;
         $pipeline = $pipelineFactory->getModel($this->params);
         return $pipeline->getPipeline($this->params["item"]);
+    }
+
+    public function savePipeline() {
+        $pipelineFactory = new \Model\Pipeline() ;
+        $pipeline = $pipelineFactory->getModel($this->params, "PipelineSaver");
+        return $pipeline->savePipeline(array("type" => "Defaults", "data" => array(
+            "project-name" => $_REQUEST["project-name"],
+            "project-description" => $_REQUEST["project-description"],
+            "default-scm-url" => $_REQUEST["default-scm-url"],
+        )));
     }
 
 }
