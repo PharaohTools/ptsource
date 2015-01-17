1e364677979f00d5661a359ff74e41e2fb7573b7
David Amanshia <phpengine@hotmail.co.uk>
2015-01-17 16:50:43 +0000

    correctly using dynaic pipelines

--- src/Modules/BuildList/Model/BuildListAllOS.php
+++ src/Modules/BuildList/Model/BuildListAllOS.php
@@ -22,7 +22,7 @@ class BuildListAllOS extends Base {
     public function getPipelines() {
         $pipelineFactory = new \Model\Pipeline() ;
         $pipeline = $pipelineFactory->getModel($this->params);
-        return $pipeline->getAllPipelines();
+        return $pipeline->getPipelines();
     }
 
 }
