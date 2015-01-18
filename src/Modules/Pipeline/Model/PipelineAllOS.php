74479abf750c1a27a9e639bc63dec34f47cb1cd7
David Amanshia <phpengine@hotmail.co.uk>
2015-01-18 23:34:24 +0000

    added create and delete pipeline methods

--- src/Modules/Pipeline/Model/PipelineAllOS.php
+++ src/Modules/Pipeline/Model/PipelineAllOS.php
@@ -38,34 +38,29 @@ class PipelineAllOS extends Base {
         return $ret ;
     }
 
-//    public function getPipeline($line) {
-//        $pipelines = $this->getPipelines() ;
-//        $ret = $pipelines[$line] ;
-//        $r = (isset($ret) && is_array($ret)) ? $ret : false ;
-//        return $r ;
-//    }
-
     public function getPipelineNames() {
         $pipelines = $this->getPipelines() ;
         $names = array_keys($pipelines) ;
         return (isset($names) && is_array($names)) ? $names : false ;
     }
+
     public function deletePipeline($name) {
         $pipelines = $this->getPipelines() ;
-        $path = dirname(dirname(__FILE__)).DS."Data".DS."demopipelines.php" ;
-        include($path) ;
-        unset($demopipelines[$name]);
-
-//        $pipelines = $this->getPipelines() ;
-//        $path = dirname(dirname(__FILE__)).DS."Data".DS."demopipelines.php" ;
-//
-//        $file = fopen($path,"w");
-//        fwrite($file,$pipelines);
-//        fclose($file);
         $ret = $pipelines[$name] ;
         $r = (isset($ret) && is_array($ret)) ? $ret : false ;
         return $r ;
-        return ;
+    }
+
+    public function createPipeline($name) {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        if (file_exists(PIPEDIR.DS.$name)) {
+            $logging->log("Directory already exists at ".PIPEDIR.DS."{$name}. Exiting with failure.", $this->getModuleName()) ;
+            return false ; }
+        else  {
+            $logging->log("Attempting to create directory ".PIPEDIR.DS."$name ", $this->getModuleName()) ;
+            $rc = self::executeAndGetReturnCode('mkdir -p '.PIPEDIR.DS.$name);
+            return $rc ; }
     }
 
 }
