12080b527b00b01a30555d12570aa2201063dc5c
Reazul Hoque <riyad.cse@workspaceit.com>
2015-01-19 17:13:35 +0600

    Add Views for searching build

---
+++ src/Modules/BuildSearch/Views/BuildListHTMLView.tpl.php
@@ -0,0 +1,63 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4 col-md-3 sidebar">
+            <div class="mini-submenu">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </div>
+            <div class="list-group sidebar-list">
+                        <span href="#" class="list-group-item active">
+                            Menu
+                            <span class="pull-right" id="slide-submenu">
+                                <i class="fa fa-times"></i>
+                            </span>
+                        </span>
+                <a href="/index.php?control=Index&action=show" class="list-group-item">
+                    <i class="fa fa-comment-o"></i> Dashboard
+                </a>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-search"></i> Configure Phrankinsense
+                </a>
+                <a href="/index.php?control=BuildConfigure&action=new" class="list-group-item">
+                    <i class="fa fa-user"></i> New Pipeline
+                </a>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-user"></i> List Pipelines
+                </a>
+                <!--
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-folder-open-o"></i> Lorem ipsum <span class="badge">14</span>
+                </a>
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-bar-chart-o"></i> Lorem ipsumr <span class="badge">14</span>
+                </a>
+                -->
+                <a href="#" class="list-group-item">
+                    <i class="fa fa-envelope"></i> History
+                </a>
+            </div>
+        </div>
+
+        <div class="col-sm-8 col-md-9 clearfix main-container">
+            <h2 class="text-uppercase text-light"><a href="/"> Phrankinsense - Pharaoh Tools</a></h2>
+
+            <div class="row clearfix no-margin">
+                <h4 class="text-uppercase text-light">Buil;t Search</h4>
+                <h3><a class="lg-anchor text-light" href="/index.php?control=BuildList&action=show">Search List <i style="font-size: 18px;" class="fa fa-chevron-right"></i></a></h3>
+
+
+            </div>
+
+            <p>
+                ---------------------------------------<br/>
+                Visit www.pharaohtools.com for more
+            </p>
+        </div>
+
+
+    </div>
+
+</div><!-- /.container -->
+
+
