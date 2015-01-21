18af4be4610508d6c3e061b6dc5682bfa53f2d52
Reazul Hoque <riyad.cse@workspaceit.com>
2015-01-21 11:26:23 +0600

    move funtionality controller to model for sign up

--- src/Modules/Signup/Model/SignupAllOS.php
+++ src/Modules/Signup/Model/SignupAllOS.php
@@ -19,14 +19,34 @@ class SignupAllOS extends Base {
         $ret="get Login";
         return $ret ;
     }
-    // @todo need to check login credential from datastore or PAM/LDAP
-    public function Checklogin($username, $password) {
-        return "aasd" ;
+
+    public function checkLogin() {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $user_info=$this->checkLoginInfo($username, $password);
+        if ($user_info == NULL) {
+            echo json_encode(array("status" => FALSE, "msg" => 'Sorry!! Wrong User name Or Password'));
+            return;
+        } else {
+            // Start the session
+            session_start();
+            $_SESSION["username"] = $username;
+            echo json_encode(array("status" => TRUE));
+            return;
+        }
     }
+    // @todo need to check login credential from datastore or PAM/LDAP
+    public function checkLoginInfo($username, $password) {
+        return $username;
+    }
+
     // @todo need to destroy all login credential or anything related to login
-    public function allLogininfodestroy() {
+    public function allLoginInfoDestroy() {
         session_destroy();
-        return;
+        // return array ("type"=>"control", "control"=>"Signup");
+        //return array ("type"=>"control", "control"=>"Signup", "action"=>"login");
+         //return array( "Signup" => array("login") );
+        header("Location: /index.php?control=Signup&action=login");
     }
 
 }
