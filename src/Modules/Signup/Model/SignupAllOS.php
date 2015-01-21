b0c6fc2b8bb6395bc1389a8dbd43464620598d48
Reazul Hoque <riyad.cse@workspaceit.com>
2015-01-21 12:08:11 +0600

    start session from autoload and login check sample login details

--- src/Modules/Signup/Model/SignupAllOS.php
+++ src/Modules/Signup/Model/SignupAllOS.php
@@ -25,19 +25,28 @@ class SignupAllOS extends Base {
         $password = $_POST['password'];
         $user_info=$this->checkLoginInfo($username, $password);
         if ($user_info == NULL) {
-            echo json_encode(array("status" => FALSE, "msg" => 'Sorry!! Wrong User name Or Password'));
+            echo json_encode(array("status" => FALSE, "msg" => "Sorry!! Wrong User name Or Password"));
             return;
         } else {
-            // Start the session
-            session_start();
-            $_SESSION["username"] = $username;
+            $_SESSION["login-status"]=TRUE;
+            $_SESSION["username"] = $user_info;
             echo json_encode(array("status" => TRUE));
             return;
         }
     }
     // @todo need to check login credential from datastore or PAM/LDAP
-    public function checkLoginInfo($username, $password) {
-        return $username;
+    public function checkLoginInfo($usr, $pass) {
+        // List of users and their password.
+        $users = array(1 => 'test1', 2 => 'test2', 3 => 'test3', 4 => 'test4');
+        $passwords = array(1 => 'e10adc3949ba59abbe56e057f20f883e', 2 => 'e10adc3949ba59abbe56e057f20f883e', 3 => 'e10adc3949ba59abbe56e057f20f883e', 4 => 'e10adc3949ba59abbe56e057f20f883e');
+        if (in_array($usr, $users) && in_array(md5($pass), $passwords))
+        {
+            return $usr;
+        }
+        else{
+            return null;
+        }
+
     }
 
     // @todo need to destroy all login credential or anything related to login
