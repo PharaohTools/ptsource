33901b22ca4f2d639b298cc0693f1469036432d4
David Amanshia <phpengine@hotmail.co.uk>
2015-02-04 21:07:30 +0000

    execute step using step type

--- src/Modules/Shell/Model/ShellLinuxUnix.php
+++ src/Modules/Shell/Model/ShellLinuxUnix.php
@@ -35,17 +35,13 @@ class ShellLinuxUnix extends Base {
     public function executeStep($step) {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
-
-        $shellDataSpellings = array("shelldata");
-        $shellScriptSpellings = array("shellscript", "shellfile");
-
-        if (in_array($step["type"], $shellDataSpellings)) {
+        if ( $step["steptype"] == "shelldata") {
             $logging->log("Running Shell from Data...") ;
             $this->executeAsShell($step["data"]) ;
             $xc = $this->executeAndLoad("$?");
             if ($xc !== 0) { return false ; }
             return true ; }
-        else if (in_array($step["type"], $shellScriptSpellings)) {
+        else if ( $step["steptype"] == "shellscript") {
             $logging->log("Running Shell from Script...") ;
             $this->executeAsShell($step["data"]) ;
             $xc = $this->executeAndLoad("$?");
