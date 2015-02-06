579b806045874d37521ffecad5b24d7a7a79372e
zahedalam <zahedalam@workspaceit.com>
2015-02-06 19:59:57 +0600

    Registration link added

--- src/Core/Base/Views/DefaultHTMLLayout.tpl.php
+++ src/Core/Base/Views/DefaultHTMLLayout.tpl.php
@@ -53,7 +53,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="/index.php?control=Signup&action=login">Login</a></li>
-                    <li><a href="#">Registration</a></li>
+                    <li><a href="/index.php?control=Signup&action=registration">Registration</a></li>
                 </ul>
 
                 <form class="navbar-form navbar-right" role="search">
