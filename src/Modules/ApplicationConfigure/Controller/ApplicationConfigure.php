b1c7a64aeab3e5f794d0d2a573a2c9088c16827b
David Amanshia <phpengine@hotmail.co.uk>
2015-02-16 02:48:01 +0000

    handling save

--- src/Modules/ApplicationConfigure/Controller/ApplicationConfigure.php
+++ src/Modules/ApplicationConfigure/Controller/ApplicationConfigure.php
@@ -9,6 +9,9 @@ class ApplicationConfigure extends Base {
         // if we don't have an object, its an array of errors
         $this->content = $pageVars ;
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        if (in_array($pageVars["route"]["action"], array("save"))) {
+            $saveModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "ApplicationConfigureSaver") ;
+            $this->content["data"]["saveState"] = $saveModel->saveAllConfigs();  }
         $this->content["data"] = $thisModel->getData();
         return array ("type"=>"view", "view"=>"applicationConfigure", "pageVars"=>$this->content);
     }
