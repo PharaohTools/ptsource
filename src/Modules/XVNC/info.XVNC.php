3e41fc3e9db2d57d3ef9f0f1192b749f0f9655b4
David Amanshia <phpengine@hotmail.co.uk>
2015-02-17 20:33:28 +0000

    added the xvnc module

---
+++ src/Modules/XVNC/info.XVNC.php
@@ -0,0 +1,48 @@
+<?php
+
+Namespace Info;
+
+class SendEmailInfo extends PTConfigureBase {
+
+    public $hidden = false;
+
+    public $name = "SendEmail Provisioner Integration";
+
+    public function _construct() {
+        parent::__construct();
+    }
+
+    public function routesAvailable() {
+        return array( "SendEmail" => array_merge(parent::routesAvailable(), array("help") ) );
+    }
+
+    public function routeAliases() {
+        return array("sendemail"=>"SendEmail");
+    }
+
+    public function events() {
+        return array("afterBuildComplete");
+    }
+
+    public function configuration() {
+        return array(
+            "smtp_server"=> array( "type" => "text", "default" => "SMTP Server", "label" => "SMTP Server Address", ),
+            "from_email"=> array( "type" => "text", "default" => "example@mail.com", "label" => "Email Address", ),
+            "username"=> array( "type" => "text", "default" => "example", "label" => "Email Username", ),
+            "password"=> array( "type" => "text", "default" => "none", "label" => "Email Password", ),
+            "port"=> array( "type" => "text", "default" => "25", "label" => "Email Server Port", ),
+        );
+    }
+
+    public function helpDefinition() {
+       $help = <<<"HELPDATA"
+    This extension provides integration with SendEmail as a Build Step. It provides code
+    functionality, but no extra CLI commands.
+
+    SendEmail
+
+HELPDATA;
+      return $help ;
+    }
+
+}
