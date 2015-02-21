777b2901d0a6803590e3048783b0b1c35c160871
David Amanshia <phpengine@hotmail.co.uk>
2015-02-21 00:20:31 +0000

    save and show actions

--- src/Modules/ModuleManager/Controller/ModuleManager.php
+++ src/Modules/ModuleManager/Controller/ModuleManager.php
@@ -9,10 +9,12 @@ class ModuleManager extends Base {
         // if we don't have an object, its an array of errors
         $this->content = $pageVars ;
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        if (in_array($pageVars["route"]["action"], array("show"))) {
+            $webModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Web") ;
+            $this->content["data"] = $webModel->getData();  }
         if (in_array($pageVars["route"]["action"], array("save"))) {
-            $saveModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "ModuleManagerSaver") ;
+            $saveModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars, "Saver") ;
             $this->content["data"]["saveState"] = $saveModel->saveAllConfigs();  }
-        $this->content["data"] = $thisModel->getData();
         return array ("type"=>"view", "view"=>"moduleManager", "pageVars"=>$this->content);
     }
 
