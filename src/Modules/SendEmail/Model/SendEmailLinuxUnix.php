05bed4103e4e77b102b56bfde14121c9754797c6
David Amanshia <phpengine@hotmail.co.uk>
2015-02-25 00:20:18 +0000

    wrap contents of event method in exception

--- src/Modules/SendEmail/Model/SendEmailLinuxUnix.php
+++ src/Modules/SendEmail/Model/SendEmailLinuxUnix.php
@@ -43,32 +43,32 @@ class SendEmailLinuxUnix extends Base {
         $logging = $loggingFactory->getModel($this->params);
         require_once dirname(dirname(__FILE__)).DS.'Libraries'.DS.'swift_required.php' ;
         // Create the Transport
-        $transport = \Swift_SmtpTransport::newInstance(
-                $this->params["build-settings"]["mod_config"]["SendEmail"]["config_smtp_server"],
-            (int) $this->params["build-settings"]["mod_config"]["SendEmail"]["config_port"])
-            ->setUsername($this->params["build-settings"]["mod_config"]["SendEmail"]["config_username"])
-            ->setPassword($this->params["build-settings"]["mod_config"]["SendEmail"]["config_password"]) ;
-        // Create the Mailer using your created Transport
-        $mailer = \Swift_Mailer::newInstance($transport);
-        // Create the message
-        $message = \Swift_Message::newInstance()
-            // Give the message a subject
-            ->setSubject($subject)
-            // Set the From address with an associative array
-            ->setFrom(array($this->params["build-settings"]["mod_config"]["SendEmail"]["from_email"] => "Pharaoh Build Server"))
-            // Set the To addresses with an associative array
-            ->setTo(array($to))
-            // Give it a body
-            ->setBody($message)
-            // And optionally an alternative body
-            //->addPart("<q>$message</q>", 'text/html')
-            // Optionally add any attachments
-            //->attach(Swift_Attachment::fromPath('my-document.pdf'))
-        ;
-
-        $logging->log ("Sending alert mail", $this->getModuleName() ) ;
-        // Send the message
         try {
+            $transport = \Swift_SmtpTransport::newInstance(
+                    $this->params["build-settings"]["mod_config"]["SendEmail"]["config_smtp_server"],
+                (int) $this->params["build-settings"]["mod_config"]["SendEmail"]["config_port"])
+                ->setUsername($this->params["build-settings"]["mod_config"]["SendEmail"]["config_username"])
+                ->setPassword($this->params["build-settings"]["mod_config"]["SendEmail"]["config_password"]) ;
+            // Create the Mailer using your created Transport
+            $mailer = \Swift_Mailer::newInstance($transport);
+            // Create the message
+            $message = \Swift_Message::newInstance()
+                // Give the message a subject
+                ->setSubject($subject)
+                // Set the From address with an associative array
+                ->setFrom(array($this->params["build-settings"]["mod_config"]["SendEmail"]["from_email"] => "Pharaoh Build Server"))
+                // Set the To addresses with an associative array
+                ->setTo(array($to))
+                // Give it a body
+                ->setBody($message)
+                // And optionally an alternative body
+                //->addPart("<q>$message</q>", 'text/html')
+                // Optionally add any attachments
+                //->attach(Swift_Attachment::fromPath('my-document.pdf'))
+            ;
+
+            $logging->log ("Sending alert mail", $this->getModuleName() ) ;
+            // Send the message
             $result = $mailer->send($message);
             if ($result == true) { $logging->log ("Email sent successfully", $this->getModuleName() ) ; }
             else { $logging->log ("Email sending error", $this->getModuleName() ) ; }
