7578df43238c9ad224f4f6e1059e132494614d81
David Amanshia <phpengine@hotmail.co.uk>
2015-02-28 21:51:22 +0000

    correct history link in menu

--- src/Modules/BuildConfigure/Views/BuildConfigureHTMLView.tpl.php
+++ src/Modules/BuildConfigure/Views/BuildConfigureHTMLView.tpl.php
@@ -23,11 +23,13 @@
                 <a href="/index.php?control=Workspace&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>" class="list-group-item">
                     <i class="fa fa-folder-open-o"></i> Workspace
                 </a>
+                <!--
                 <a href="#" class="list-group-item">
                     <i class="fa fa-bar-chart-o"></i> Monitors <span class="badge">6</span>
                 </a>
-                <a href="#" class="list-group-item">
-                    <i class="fa fa-bar-chart-o"></i> History <span class="badge">3</span>
+                -->
+                <a href="/index.php?control=PipeRunner&action=history&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>" class="list-group-item">
+                    <i class="fa fa-bar-chart-o"></i> History <span class="badge"><?php echo $pageVars["data"]["history_count"] ; ?></span>
                 </a>
                 <a href="/index.php?control=BuildHome&action=delete&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>" class="list-group-item">
                     <i class="fa fa-envelope"></i> Delete
