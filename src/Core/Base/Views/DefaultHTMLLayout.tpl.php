1e2c695b774d7790b371c726ede46148d66d075a
kevells <kevell.corp@gmail.com>
2015-03-12 20:26:11 +0530

    New UI for Loging, Registration and Dashboard pages

--- src/Core/Base/Views/DefaultHTMLLayout.tpl.php
+++ src/Core/Base/Views/DefaultHTMLLayout.tpl.php
@@ -1,143 +1,163 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="Assets/startbootstrap-sb-admin-2-1.0.5/Assets/startbootstrap-sb-admin-2-1.0.5/favicon.ico">
 
-    <title>PTBuild - Pharaoh Tools</title>
+		<title>PTBuild - Pharaoh Tools</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="Assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="Assets/css/style.css" rel="stylesheet">
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- Bootstrap Core CSS -->
+		<link href="Assets/startbootstrap-sb-admin-2-1.0.5/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
+		<!-- MetisMenu CSS -->
+		<link href="Assets/startbootstrap-sb-admin-2-1.0.5/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<!-- Timeline CSS -->
+		<link href="Assets/startbootstrap-sb-admin-2-1.0.5/dist/css/timeline.css" rel="stylesheet">
 
+		<!-- Custom CSS -->
+		<link href="Assets/startbootstrap-sb-admin-2-1.0.5/dist/css/sb-admin-2.css" rel="stylesheet">
 
-    <![endif]-->
-    <!--  sign up add -->
+		<!-- Custom Fonts -->
+		<link href="Assets/startbootstrap-sb-admin-2-1.0.5/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <script src="/index.php?control=AssetLoader&action=show&module=Signup&type=js&asset=signup.js"></script>
-    <script src="/index.php?control=AssetLoader&action=show&module=PostInput&type=js&asset=jquery.min.js"></script>
-    <script src="/index.php?control=AssetLoader&action=show&module=PostInput&type=js&asset=jquery-ui.min.js"></script>
-    <style>
-        body{
-            padding-top: 72px;
-        }
-    </style>
-</head>
+		<script src="Assets/startbootstrap-sb-admin-2-1.0.5/bower_components/jquery/dist/jquery.min.js"></script>
 
-<body>
-<input type="hidden" id="base_url" value="http://www.ptbuild.tld">
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a href="/index.php?control=Index&action=show"><img src="/index.php?control=AssetLoader&action=show&module=PostInput&type=image&asset=5.png" class="navbar-img" /></a>
-            <a class="navbar-brand" href="/index.php?control=Index&action=show">PTBuild</a>
-        </div>
+		<!-- Bootstrap Core JavaScript --
+		<script src="Assets/startbootstrap-sb-admin-2-1.0.5/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>-->
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <?php if($pageVars["route"]["control"] == "Signup"){ ?>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="/index.php?control=Signup&action=login">Login</a></li>
-                    <li><a href="/index.php?control=Signup&action=registration">Registration</a></li>
-                </ul>
+		<!-- Metis Menu Plugin JavaScript -->
+		<script src="Assets/startbootstrap-sb-admin-2-1.0.5/bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
-                <form class="navbar-form navbar-right" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-info">Submit</button>
-                </form>
+		<!-- Custom Theme JavaScript 
+		<script src="Assets/startbootstrap-sb-admin-2-1.0.5/dist/js/sb-admin-2.js"></script>-->
 
-            </div><!-- /.navbar-collapse -->
-        <?php }else{?>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <!--
-                    <li class="active"><a href="/index.php?control=BuildConfigure&action=new">New Pipeline</a></li>
-                    <li><a href="#">History</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Others <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="/index.php?control=Signup&action=logout">Logout</a></li>
-                    -->
-                </ul>
-                <form class="navbar-form navbar-right" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-info">Submit</button>
-                </form>
-            </div><!-- /.navbar-collapse -->
-        <?php } ?>
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
 
-    </div><!-- /.container-fluid -->
-</nav>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 
-<?php echo $this->renderMessages($pageVars); ?>
-<?php echo $templateData; ?>
+		<![endif]-->
+		<!--  sign up add -->
+		<script src="Assets/Signup/js/signup.js"></script>
+		<script src="Assets/startbootstrap-sb-admin-2-1.0.5/bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="Assets/js/jquery-ui.min.js"></script>
+		<style>
+			body {
+				padding-top: 72px;
+			}
+		</style>
+	</head>
 
+	<body>
+		<input type="hidden" id="base_url" value="http://www.ptbuild.tld">
+<?php
+	if($pageVars["route"]["action"] != "registration" && $pageVars["route"]["action"] != "login") { ?>
+		<div id="wrapper">
+			<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-top: -70px">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-" href="/index.php?control=Index&action=show"><img src="/Assets/images/5.png" class="navbar-img" style="height: 60px;padding: 5px" /><b>PTBuild</b></a>
+				</div>
+				<!-- /.navbar-header -->
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="/index.php?control=AssetLoader&action=show&module=PostInput&type=js&asset=bootstrap.min.js"></script>
-<script>
-    $(function() {
-        $('#slide-submenu').on('click', function() {
-            $(this).closest('.list-group').fadeOut('slide', function() {
-                $('.mini-submenu').fadeIn();
-            });
-        });
+				<ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-bell fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-alerts">
+                        <li>
+                            <a href="#">
+                                <div>
+                                    <i class="fa fa-comment fa-fw"></i> New Comment
+                                    <span class="pull-right text-muted small">4 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                       
+                        <li class="divider"></li>
+                        <li>
+                            <a class="text-center" href="#">
+                                <strong>See All Alerts</strong>
+                                <i class="fa fa-angle-right"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-alerts --
+                </li>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="/index.php?control=Signup&action=logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>   
+            <!-- /.navbar-top-links -->
+			</nav>
+<?php } ?>
+			<?php echo $this -> renderMessages($pageVars); ?>
+			<?php echo $templateData; ?>
 
-        $('.mini-submenu').on('click', function() {
-            $(this).next('.list-group').toggle('slide');
-            $('.mini-submenu').hide();
-        });
+			<!-- Bootstrap core JavaScript
+			================================================== -->
+			<!-- Placed at the end of the document so the pages load faster -->
+			<script src="/Assets/js/bootstrap.min.js"></script>
+			<script>
+				$(function() {
+					$('#slide-submenu').on('click', function() {
+						$(this).closest('.list-group').fadeOut('slide', function() {
+							$('.mini-submenu').fadeIn();
+						});
+					});
 
-        $.ajax({
-            type: 'POST',
-            url: $('#base_url').val() + '/index.php?control=Signup&action=login-status',
-            data: {
-                url:document.URL
-            },
-            dataType: "json",
-            success: function(result)
-            {
-                if(result.status == false){
-                    window.location.assign($('#base_url').val() + '/index.php?control=Signup&action=login');
-                }
-            }
-        });
-    })
+					$('.mini-submenu').on('click', function() {
+						$(this).next('.list-group').toggle('slide');
+						$('.mini-submenu').hide();
+					});
 
+					$.ajax({
+						type : 'POST',
+						url : $('#base_url').val() + '/index.php?control=Signup&action=login-status',
+						data : {
+							url : document.URL
+						},
+						dataType : "json",
+						success : function(result) {
+							if (result.status == false) {
+								window.location.assign($('#base_url').val() + '/index.php?control=Signup&action=login');
+							}
+						}
+					});
+				})
 
-</script>
-</body>
+			</script>
+	</body>
 </html>
--- src/Modules/Index/Views/IndexHTMLView.tpl.php
+++ src/Modules/Index/Views/IndexHTMLView.tpl.php
@@ -1,74 +1,276 @@
-<div class="container">
-    <div class="row">
-        <div class="col-sm-4 col-md-3 sidebar">
-            <div class="mini-submenu">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
+<div class="container" id="wrapper">
+
+	<div class="navbar-default sidebar" role="navigation">
+		<div class="sidebar-nav navbar-collapse">
+			<ul class="nav in" id="side-menu">
+				<li class="sidebar-search">
+					<div class="input-group custom-search-form">
+						<input type="text" class="form-control" placeholder="Search...">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="button">
+								<i class="fa fa-search"></i>
+							</button> </span>
+					</div>
+					<!-- /input-group -->
+				</li>
+				<li>
+					<a href="/index.php?control=Index&action=show" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+				</li>
+				<li>
+					<a href="/index.php?control=ApplicationConfigure&action=show"><i class="fa fa-sitemap fa-fw"></i> Configure PTBuild<span class="fa arrow"></span></a>
+					<ul class="nav nav-second-level collapse">
+						<li>
+							<a href="flot.html">New Pipeline</a>
+						</li>
+						<li>
+							<a href="morris.html">Morris.js Charts</a>
+						</li>
+					</ul>
+					<!-- /.nav-second-level -->
+				</li>
+				<li>
+					<a href="/index.php?control=BuildConfigure&action=new"><i class="fa fa-table fa-fw"></i> New Pipeline</a>
+				</li>
+				<li>
+					<a href="/index.php?control=BuildList&action=show"><i class="fa fa-edit fa-fw"></i> All Pipeline</a>
+				</li>
+				<li>
+					<a href="/index.php?control=Monitors&action=DefaultHistory"><i class="fa fa-history fa-fw"></i> History<span class="fa arrow"></span></a>
+					<!-- <ul class="nav nav-second-level collapse">
+					<li>
+					<a href="panels-wells.html">Panels and Wells</a>
+					</li>
+					<li>
+					<a href="buttons.html">Buttons</a>
+					</li>
+					<li>
+					<a href="notifications.html">Notifications</a>
+					</li>
+					<li>
+					<a href="typography.html">Typography</a>
+					</li>
+					<li>
+					<a href="icons.html"> Icons</a>
+					</li>
+					<li>
+					<a href="grid.html">Grid</a>
+					</li>
+					</ul>
+					<!-- /.nav-second-level -->
+				</li>
+				<!-- <li>
+				<a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+				<ul class="nav nav-second-level collapse">
+				<li>
+				<a href="#">Second Level Item</a>
+				</li>
+				<li>
+				<a href="#">Second Level Item</a>
+				</li>
+				<li>
+				<a href="#">Third Level <span class="fa arrow"></span></a>
+				<ul class="nav nav-third-level collapse">
+				<li>
+				<a href="#">Third Level Item</a>
+				</li>
+				<li>
+				<a href="#">Third Level Item</a>
+				</li>
+				<li>
+				<a href="#">Third Level Item</a>
+				</li>
+				<li>
+				<a href="#">Third Level Item</a>
+				</li>
+				</ul>-->
+				<!-- /.nav-third-level -->
+				</li>
+			</ul>
+			<!-- /.nav-second-level -->
+			<!-- </li>
+			<li>
+			<a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+			<ul class="nav nav-second-level collapse">
+			<li>
+			<a href="blank.html">Blank Page</a>
+			</li>
+			<li>
+			<a href="login.html">Login Page</a>
+			</li>
+			</ul>-->
+			<!-- /.nav-second-level -->
+			</li>
+			</ul>
+		</div>
+		<!-- /.sidebar-collapse -->
+	</div>
+
+	<!-- <div class="navbar-default sidebar">
+	<div class="sidebar-nav navbar-collapse">
+	<span href="#" class="list-group-item active">
+	Menu
+	<span class="pull-right" id="slide-submenu">
+	<i class="fa fa-times"></i>
+	</span>
+	</span>
+	<li>
+	<a href="/index.php?control=Index&action=show" class="list-group-item">
+	<span class="fa fa-dashboard fa-fw"></span> Dashboard
+	</li>
+	</a>
+	<li>
+	<a href="/index.php?control=ApplicationConfigure&action=show" class="list-group-item">
+	<span class="fa fa-search"></span> Configure PTBuild
+	</li>
+	</a>
+	<li>
+	<a href="/index.php?control=BuildConfigure&action=new" class="list-group-item">
+	<span class="fa fa-user"></span> New Pipeline
+	</li>
+	</a>
+	<li>
+	<a href="/index.php?control=BuildList&action=show" class="list-group-item">
+	<span class="fa fa-user"></span> All Pipelines
+	</li>
+	</a>
+	<li>
+	<a href="/index.php?control=Monitors&action=DefaultHistory" class="list-group-item">
+	<span class="fa fa-bar-chart-o"></span> History
+	</li>
+	</a>
+	</div>-->
+</div>
+</div>
+
+
+<div class="col-sm-8 col-md-9" id="page-wrapper">
+	<h4 class="text-uppercase text-light">Pharaoh Tools</h4>
+	<div class="row clearfix no-margin">
+		<div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-cog fa-spin fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">100</div>
+                                    <div>All!</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-check-circle fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">50</div>
+                                    <div>All sucess!</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-times-circle fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">20</div>
+                                    <div>All Failed!</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-chain-broken fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">30</div>
+                                    <div>All Unstable!</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="list-group sidebar-list">
-                <span href="#" class="list-group-item active">
-                    Menu
-                    <span class="pull-right" id="slide-submenu">
-                        <i class="fa fa-times"></i>
-                    </span>
-                </span>
-                <a href="/index.php?control=Index&action=show" class="list-group-item">
-                    <i class="fa fa-comment-o"></i> Dashboard
-                </a>
-                <a href="/index.php?control=ApplicationConfigure&action=show" class="list-group-item">
-                    <i class="fa fa-search"></i> Configure PTBuild
-                </a>
-                <a href="/index.php?control=BuildConfigure&action=new" class="list-group-item">
-                    <i class="fa fa-user"></i> New Pipeline
-                </a>
-                <a href="/index.php?control=BuildList&action=show" class="list-group-item">
-                    <i class="fa fa-user"></i> All Pipelines
-                </a>
-                <a href="/index.php?control=Monitors&action=DefaultHistory" class="list-group-item">
-                    <i class="fa fa-bar-chart-o"></i> History
-                </a>
-            </div>
-        </div>
+		<h3><a class="lg-anchor text-light" href=""> PTBuild - The Builder <i style="font-size: 18px;" class="fa fa-chevron-right"></i></a></h3>
+		<p>
+			Build and Monitoring Server in PHP.
+			<br/>
+			Create simple or complex build pipelines fully integrated with pharaoh tools
+			<br/>
+			Create monitoring application features in minutes.
+			<br/>
+			Using Convention over Configuration, a lot of common build tasks can be completed with little or
+			no extra implementation work.
+		</p>
+	</div>
+	<hr>
+	<div class="row clearfix no-margin">
+		<h3> Available Modules: <i style="font-size: 18px;" class="fa fa-chevron-right"></i></h3>
+		<p>
+			---------------------------------------
+		</p>
+		<?php
 
-        <div class="col-sm-8 col-md-9 clearfix main-container">
-            <h4 class="text-uppercase text-light">Pharaoh Tools</h4>
-            <div class="row clearfix no-margin">
-                <h3>
-                    <a class="lg-anchor text-light" href="">
-                        PTBuild - The Builder <i style="font-size: 18px;" class="fa fa-chevron-right"></i></a></h3>
-                <p>
-                    Build and Monitoring Server in PHP.
-                    <br/>
-                    Create simple or complex build pipelines fully integrated with pharaoh tools
-                    <br/>
-                    Create monitoring application features in minutes.
-                    <br/>
-                    Using Convention over Configuration, a lot of common build tasks can be completed with little or
-                    no extra implementation work.
-                </p>
-            </div>
-            <hr>
-            <div class="row clearfix no-margin">
-                <h3> Available Modules: <i style="font-size: 18px;" class="fa fa-chevron-right"></i></h3>
-                <p> --------------------------------------- </p>
-                <?php
+		foreach ($pageVars["modulesInfo"] as $moduleInfo) {
+			if ($moduleInfo["hidden"] != true) {
+				echo '<p>' . $moduleInfo["command"] . ' - ' . $moduleInfo["name"] . "</p>";
+			}
+		}
+		?>
 
-                foreach ($pageVars["modulesInfo"] as $moduleInfo) {
-                    if ($moduleInfo["hidden"] != true) {
-                        echo '<p>'.$moduleInfo["command"].' - '.$moduleInfo["name"]."</p>";
-                    }
-                }
+		<p>
+			---------------------------------------
+			<br/>
+			Visit www.pharaohtools.com for more
+		</p>
+	</div>
 
-                ?>
+</div>
 
-                <p>
-                    ---------------------------------------<br/>
-                    Visit www.pharaohtools.com for more
-                </p>
-            </div>
-
-        </div>
-
-    </div>
+</div>
 </div><!-- /.container -->
