671ba05019d8471b2eef39cc9486b0f1b1020aa8
David Amanshia <phpengine@hotmail.co.uk>
2015-03-15 19:31:37 +0000

    registration has its own js filw

--- src/Modules/Signup/Assets/js/registration.js
+++ src/Modules/Signup/Assets/js/registration.js
@@ -1,47 +1,3 @@
-/**
- * Created by riyad on 1/21/15.
- */
-
-function submit_login() {
-    $('#login_error_msg').html('');
-    $('#login_username_alert').html('');
-    $('#login_password_alert').html('');
-
-    if ($('#login_username').val() == '') {
-        $('#login_username_alert').html('&nbsp;&nbsp;Please enter your User Name');
-        $('#login_username').focus();
-        return;
-    }
-
-    if ($('#login_password').val() == '') {
-        $('#login_password_alert').html('&nbsp;&nbsp;Please enter your Password');
-        $('#login_password').focus();
-        return;
-    }
-    $.ajax({
-        type: 'POST',
-        url: $('#base_url').val() + '/index.php?control=Signup&action=login-submit',
-        data: {
-            username:$('#login_username').val(),
-            password:$('#login_password').val()
-        },
-        dataType: "json",
-        success: function(result)
-        {
-            if(result.status == true){
-                window.location.assign($('#base_url').val() + '/index.php?control=Index&action=show');
-            }
-            else{
-                $('#login_error_msg').html('&nbsp;&nbsp;'+result.msg);
-                $('#login_username').focus();
-                return;
-            }
-
-        }
-    });
-
-}
-
 function submit_registration() {
     $('#login_error_msg').html('');
     $('#login_username_alert').html('');
@@ -52,41 +8,33 @@ function submit_registration() {
     if ($('#login_username').val() == '') {
         $('#login_username_alert').html('&nbsp;&nbsp;Please enter your User Name');
         $('#login_username').focus();
-        return;
-    }
+        return; }
 
     if ($('#login_email').val() == '') {
         $('#login_email_alert').html('&nbsp;&nbsp;Please enter your Email');
         $('#login_email').focus();
-        return;
-    }
+        return; }
 
     if (!validateEmail($('#login_email').val())) {
         $('#login_email_alert').html('&nbsp;&nbsp;Format is not correct.');
         $('#login_email').focus();
-        return;
-    }
+        return; }
 
     if ($('#login_password').val() == '') {
         $('#login_password_alert').html('&nbsp;&nbsp;Please enter your Password');
         $('#login_password').focus();
-        return;
-    }
+        return;  }
 
     if ($('#login_password_match').val() == '') {
         $('#login_password_match_alert').html('&nbsp;&nbsp;Please enter Password Again');
         $('#login_password_match').focus();
-        return;
-    }
+        return; }
 
     if ($('#login_password_match').val() != $('#login_password').val()) {
         $('#login_password_match_alert').html('&nbsp;&nbsp;Password Does Not Match...Try Again');
         $('#login_password_match').val('');
         $('#login_password_match').focus();
-        return;
-    }
-
-
+        return; }
 
     $.ajax({
         type: 'POST',
@@ -97,23 +45,16 @@ function submit_registration() {
             password:$('#login_password').val()
         },
         dataType: "json",
-        success: function(result)
-        {
-
-                $('#'+result.id).html('&nbsp;&nbsp;'+result.msg);
-                $('#'+result.id).focus();
-
-
-        }
+        success: function(result) {
+                $('#registration_error_msg').html('&nbsp;&nbsp;'+result.msg);
+                $('#registration_error_msg').focus(); }
     });
+
+    alert("reg sent") ;
 
 }
 
-function validateEmail($email) {
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    if( !emailReg.test( $email ) ) {
-        return false;
-    } else {
-        return true;
-    }
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
