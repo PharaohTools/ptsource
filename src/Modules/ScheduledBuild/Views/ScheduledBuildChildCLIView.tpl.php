25e9f8217a4f6a1d9609fdccb104c1e1519acbc2
David Amanshia <phpengine@hotmail.co.uk>
2015-03-18 00:48:51 +0000

    scheduled build finds correct waiting pipes

--- src/Modules/ScheduledBuild/Views/ScheduledBuildChildCLIView.tpl.php
+++ src/Modules/ScheduledBuild/Views/ScheduledBuildChildCLIView.tpl.php
@@ -1,2 +1,9 @@
 <?php
-echo "" ;
+
+echo "Executing Scheduled Builds\n" ;
+
+if (count($pageVars["data"]["scheduled"])>0) {
+    foreach ($pageVars["data"]["scheduled"] as $pipe) {
+        echo $pipe["project-name"]."\n" ; } }
+else {
+    echo "No Builds scheduled to run now\n" ; }
