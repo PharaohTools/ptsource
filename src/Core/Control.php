0ea50835b42ad6d28d0cd5a108f1af4c61cc1bf1
David Amanshia <phpengine@hotmail.co.uk>
2015-03-24 02:42:42 +0000

    dont block scheduled build from non logged in user

--- src/Core/Control.php
+++ src/Core/Control.php
@@ -5,10 +5,19 @@ Namespace Core ;
 class Control {
 
     public function executeControl($control, $pageVars) {// print_r($pageVars);
+
+
+//        $eventRunnerFactory = new \Model\EventRunner() ;
+//        $eventRunner = $eventRunnerFactory->getModel($this->params) ;
+//        $ev = $eventRunner->eventRunner("prepareBuild") ;
+//        if ($ev == false) { return $this->failBuild() ; }
+
+        // @todo use the above code here instead, to just run an event
+
     	$userPermissionClass = '\\Controller\\UserPermission';
 		$userPermissionControlObject = new $userPermissionClass;
 		session_start();
-		if (!in_array($pageVars['route']['control'], array('Signup', 'OAuth', 'LDAP', 'AssetLoader', 'PipeRunner'))) {
+		if (!in_array($pageVars['route']['control'], array('Signup', 'OAuth', 'LDAP', 'AssetLoader', 'PipeRunner', 'ScheduledBuild'))) {
 			if ($_SESSION["login-status"] === TRUE) {
 				if ($userPermissionControlObject->execute($pageVars) == false) {
 					$control = 'index';
