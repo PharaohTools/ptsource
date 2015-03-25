e7a5931ed35e35e566857189fe2c798f7b22cc6b
kevellcorp <kevell.corp@gmail.com>
2015-03-25 20:21:45 +0530

    css edit

--- src/Modules/PipeRunner/Views/PipeRunnerHTMLView.tpl.php
+++ src/Modules/PipeRunner/Views/PipeRunnerHTMLView.tpl.php
@@ -3,13 +3,13 @@
 		<div class="sidebar-nav navbar-collapse">
 			<ul class="nav in" id="side-menu">
 				<li class="sidebar-search">
-					<div class="input-group custom-search-form hvr-bounce-in">
+					<div class="input-group custom-search-form  hvr-bounce-in">
 						<input type="text" class="form-control" placeholder="Search...">
 						<span class="input-group-btn">
 							<button class="btn btn-default" type="button">
 								<i class="fa fa-search"></i>
-							</button>
-                        </span>
+							</button> </span>
+                        
 					</div>
 					</li>
                 <li>
