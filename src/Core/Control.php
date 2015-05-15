c6abf20b49fc0b07598b7a03b28995419595409d
David Amanshia <phpengine@hotmail.co.uk>
2015-05-15 12:41:21 +0100

    updating auth event call to check all results

--- src/Core/Control.php
+++ src/Core/Control.php
@@ -4,37 +4,62 @@ Namespace Core ;
 
 class Control {
 
-    public function executeControl($control, $pageVars) {// print_r($pageVars);
+    public function executeControl($control, $pageVars) {
+        $eventRunnerFactory = new \Model\EventRunner() ;
+        $eventRunner = $eventRunnerFactory->getModel($pageVars) ;
+        $ev = $eventRunner->eventRunner("authenticate", true) ;
+        // @todo lets get the below from a model, overridable
+        $handled = $this->handleAuthenticationResponse($ev, $control, $pageVars) ;
+        $control = $handled["control"] ;
+        $pageVars = $handled["pageVars"] ;
+        $className = '\\Controller\\'.ucfirst($control);
+        $controlObject = new $className;
+        return $controlObject->execute($pageVars);
+    }
 
+    // @todo everything below this line should probably be ion a model, or at least a helper
+    private function handleAuthenticationResponse($ev, $control, $pageVars) {
+        $handled = array() ;
+        $handled["control"] = $control ;
+        $handled["pageVars"] = $pageVars ;
+        // if we have failed authentication, we will have a false
+        if (in_array(false, $ev)) {
+            error_log("failed authentication, forcing back to signup module") ;
+            $handled["control"] = "Signup";
+            $handled["pageVars"]["route"]["control"] = "Signup" ;
+            return $handled ; }
+        // at this point we are authenticaed, so lets check authorization too
+        $handled = $this->handleAuthorizationResponse($ev, $handled["control"], $handled["pageVars"]) ;
+        return $handled  ;
+    }
 
-//        $eventRunnerFactory = new \Model\EventRunner() ;
-//        $eventRunner = $eventRunnerFactory->getModel($this->params) ;
-//        $ev = $eventRunner->eventRunner("prepareBuild") ;
-//        if ($ev == false) { return $this->failBuild() ; }
+    private function handleAuthorizationResponse($ev, $control, $pageVars) {
+        $handled = array() ;
+        $handled["pageVars"] = $pageVars ;
+        $umf = new \Model\UserManager();
+        $um = $umf->getModel(array());
 
-        // @todo use the above code here instead, to just run an event
-
-    	$userPermissionClass = '\\Controller\\UserPermission';
-		$userPermissionControlObject = new $userPermissionClass;
-		session_start();
-		if (!in_array($pageVars['route']['control'], array('Signup', 'OAuth', 'LDAP', 'AssetLoader', 'PipeRunner', 'ScheduledBuild'))) {
-			if ($_SESSION["login-status"] === TRUE) {
-				if ($userPermissionControlObject->execute($pageVars) == false) {
-					$control = 'index';
-					$pageVars['route']['control'] = 'index';
-					$pageVars['route']['action'] = 'index';
-				}
-			}
-			else {
-				$control = 'Signup';
-				$pageVars['route']['control'] = 'Signup';
-				$pageVars['route']['action'] = 'login';
-			}
-		}
-		$className = '\\Controller\\'.ucfirst($control);
-    	$controlObject = new $className;
-		return $controlObject->execute($pageVars);
-		
+        // the user is logged in, but resticted
+        $restricted = $um->getRestrictionStatus() ;
+        if ($restricted === true) {
+            error_log("restricted user authentication, forcing back to signup module") ;
+            $handled["control"] = "Signup";
+            $handled["pageVars"]["route"]["control"] = "Signup" ;
+            return $handled ; }
+        $myRole = $um->getMyUserRoleId() ;
+        // if we are a client use client
+        if ($myRole==4) {
+            $clientAllowedList = array(
+                "ClientPortal", "ClientJobList", "ClientJobHome", "ClientDocuments", "PostInput",
+                "AssetLoader", "Signup", "About"
+            ) ;
+            if (!in_array($control, $clientAllowedList)) {
+                $handled["control"] = "ClientPortal";
+                $handled["pageVars"]["route"]["control"] = "ClientPortal" ;
+                return $handled ; } }
+        $handled["control"] = $control;
+        $handled["pageVars"]["route"]["control"] = $control ;
+        return $handled ;
     }
 
 }
