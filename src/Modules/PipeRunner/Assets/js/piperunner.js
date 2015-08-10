7dd530fe3c54420a7a622b4ad93cd2def4056a62
David Amanshia <phpengine@pharaohtools.com>
2015-08-10 03:32:13 +0100

    piperunner js has default page update interval of 4 seconds

--- src/Modules/PipeRunner/Assets/js/piperunner.js
+++ src/Modules/PipeRunner/Assets/js/piperunner.js
@@ -1,6 +1,6 @@
 done = false ;
 max = 0 ;
-window.outUpdater = setInterval(function () { updatePage() }, 1000);
+window.outUpdater = setInterval(function () { updatePage() }, 4000);
 
 function updatePage() {
     console.log("running update page js method");
