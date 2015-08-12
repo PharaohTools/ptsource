b257a4a8a2161538fdc832dc8960f54aff7cf621
David Amanshia <phpengine@pharaohtools.com>
2015-08-12 11:52:59 +0100

    pipe feature repository working

--- src/Modules/PipeFeature/Model/PipeFeatureRepositoryAllOS.php
+++ src/Modules/PipeFeature/Model/PipeFeatureRepositoryAllOS.php
@@ -15,22 +15,22 @@ class PipeFeatureRepositoryAllOS extends Base {
     public $modelGroup = array("PipeFeatureRepository") ;
 
     public function getAllPipeFeatures() {
-        $builders = array();
+        $pipeFeatures = array();
         $names = $this->getPipeFeatureNames() ;
-        $builderFactory = new \Model\PipeFeature() ;
-        $builder = $builderFactory->getModel($this->params);
+        $pipeFeatureFactory = new \Model\PipeFeature() ;
+        $pipeFeature = $pipeFeatureFactory->getModel($this->params);
         foreach ($names as $name) {
-            $builders[$name] = $builder->getPipeFeature($name); }
-        return $builders ;
+            $pipeFeatures[$name] = $pipeFeature->getPipeFeature($name); }
+        return $pipeFeatures ;
     }
 
     public function getAllPipeFeaturesFormFields() {
         $formFields = array();
         $names = $this->getPipeFeatureNames() ;
-        $builderFactory = new \Model\PipeFeature() ;
-        $builder = $builderFactory->getModel($this->params);
+        $pipeFeatureFactory = new \Model\PipeFeature() ;
+        $pipeFeature = $pipeFeatureFactory->getModel($this->params);
         foreach ($names as $name) {
-            $bo = $builder->getPipeFeature($name);
+            $bo = $pipeFeature->getPipeFeature($name);
             $formFields[$name] = $bo["fields"] ; }
         return $formFields ;
     }
@@ -38,26 +38,26 @@ class PipeFeatureRepositoryAllOS extends Base {
     public function getStepPipeFeaturesFormFields() {
         $formFields = array();
         $names = $this->getPipeFeatureNames(array("buildSteps")) ;
-        $builderFactory = new \Model\PipeFeature() ;
-        $builder = $builderFactory->getModel($this->params);
+        $pipeFeatureFactory = new \Model\PipeFeature() ;
+        $pipeFeature = $pipeFeatureFactory->getModel($this->params);
         foreach ($names as $name) {
-            $bo = $builder->getPipeFeature($name);
+            $bo = $pipeFeature->getPipeFeature($name);
             $formFields[$name] = $bo["fields"] ; }
         return $formFields ;
     }
 
     public function getPipeFeatureNames($includeTypes = array()) {
-        $builderNames = array() ;
+        $pipeFeatureNames = array() ;
         $infos = \Core\AutoLoader::getInfoObjects() ;
-        $types = (count($includeTypes)>0) ? $includeTypes : array( "buildSteps", "buildSettings", "events") ;
+        $types = (count($includeTypes)>0) ? $includeTypes : array("pipeFeatures") ;
         foreach ($infos as $info) {
             foreach ($types as $type) {
                 if ( method_exists($info, $type)) {
                     $name = get_class($info);
                     $name = str_replace("Info\\", "", $name) ;
                     $name = substr($name, 0, strlen($name)-4) ;
-                    $builderNames[] = $name; } } }
-        return $builderNames ;
+                    $pipeFeatureNames[] = $name; } } }
+        return $pipeFeatureNames ;
     }
 
 }
