b5bc1ca2cd7cffcf6c030e89b01f3f91f1ba047a
David Amanshia <phpengine@pharaohtools.com>
2015-08-18 23:31:49 +0100

    build configure supports copy action

--- src/Modules/BuildConfigure/Controller/BuildConfigure.php
+++ src/Modules/BuildConfigure/Controller/BuildConfigure.php
@@ -9,9 +9,17 @@ class BuildConfigure extends Base {
         // if we don't have an object, its an array of errors
         $this->content = $pageVars ;
         if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+        if (in_array($pageVars["route"]["action"], array("copy"))) {
+            if ($thisModel->saveCopiedPipeline()==true) {
+                $this->content["route"]["action"] = "show" ;
+                $this->content["item"] = $_REQUEST["source_pipeline"] ;
+                return array ("type"=>"control", "control"=>"buildConfigure", "pageVars"=>$this->content); }
+            $this->content["data"] = $thisModel->getCopyData();
+            return array ("type"=>"view", "view"=>"buildConfigureCopy", "pageVars"=>$this->content); }
         if (in_array($pageVars["route"]["action"], array("save"))) {
             $this->content["data"]["saveState"] = $thisModel->savePipeline();  }
-        $this->content["data"] = $thisModel->getData();
+        if (in_array($pageVars["route"]["action"], array("new", "show"))) {
+            $this->content["data"] = $thisModel->getData(); }
         return array ("type"=>"view", "view"=>"buildConfigure", "pageVars"=>$this->content);
     }
 
