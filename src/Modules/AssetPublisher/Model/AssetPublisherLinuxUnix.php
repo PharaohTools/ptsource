a7a2b218796f675ad32e6f0334dbdcaa4bbc8614
David Amanshia <phpengine@pharaohtools.com>
2015-08-28 10:43:06 +0100

    module in logging

--- src/Modules/AssetPublisher/Model/AssetPublisherLinuxUnix.php
+++ src/Modules/AssetPublisher/Model/AssetPublisherLinuxUnix.php
@@ -31,26 +31,27 @@ class AssetPublisherLinuxUnix extends Base {
         $logging = $loggingFactory->getModel($this->params);
         $modDir = "/opt/".PHARAOH_APP."/".PHARAOH_APP."/src/Modules/" ;
         $mods = scandir($modDir) ;
+        $mods = array_diff($mods, array(".", "..")) ;
         $piDir = "/opt/".PHARAOH_APP."/".PHARAOH_APP."/src/Modules/PostInput/" ;
         $statuses = array();
         foreach ($mods as $mod) {
             $modPath = $modDir.$mod ;
-            $logging->log("Looking for Asset directory in Module $mod") ;
+            $logging->log("Looking for Asset directory in Module $mod", $this->getModuleName()) ;
             $modAssets = $modPath.DS."Assets" ;
             if (is_dir($modPath.DS."Assets")) {
                 if ($mod == "PostInput") {
-                    $logging->log("Ignoring Asset directory in Module $mod") ;
+                    $logging->log("Ignoring Asset directory in Module $mod", $this->getModuleName()) ;
                     continue ; }
                 else {
-                    $logging->log("Found Asset directory in Module $mod") ;
+                    $logging->log("Found Asset directory in Module $mod", $this->getModuleName()) ;
                     $modPublicAssetDir = $piDir.'Assets'.DS.'Modules'.DS.$mod ; }
                 if (!is_dir($modPublicAssetDir)) {
-                    $logging->log("Creating Web server readable Assets directory $modPublicAssetDir") ;
+                    $logging->log("Creating Web Server Assets directory $modPublicAssetDir", $this->getModuleName()) ;
                     $comm = "mkdir -p $modPublicAssetDir" ;
                     $status = $this->executeAndGetReturnCode($comm);
                     if ($status !== 0) {
                         return false; } }
-                $logging->log("Copying Assets to Web server readable directory ") ;
+                $logging->log("Copying Assets to Web server readable directory", $this->getModuleName()) ;
                 $comm = "cp -r $modAssets".DS."* $modPublicAssetDir" ;
                 $statuses[] = $this->executeAndGetReturnCode($comm); } }
         if (in_array(false, $statuses)) { return false; }
