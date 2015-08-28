1468516849d812ae1dbb2248a1ae68e706c09ca3
David Amanshia <phpengine@pharaohtools.com>
2015-08-28 10:44:24 +0100

    todos and formatting

--- src/Modules/Signup/Controller/Signup.php
+++ src/Modules/Signup/Controller/Signup.php
@@ -2,36 +2,30 @@
 
 Namespace Controller;
 
-class Signup extends Base
-{
+class Signup extends Base {
 
-    public function execute($pageVars)
-    {
+    public function execute($pageVars) {
+
         $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars);
         // if we don't have an object, its an array of errors
         $this->content = $pageVars;
         if (is_array($thisModel)) {
             return $this->failDependencies($pageVars, $this->content, $thisModel); }
 
+        // @todo if signup is not enabled this should redirect to the Index module
         // @todo This is functionality. It should be in the Model, not here
-        // @todo do not Start the session here. At the very least, this should be in a wrapper
-        // like $session->ensureSession();
-        
         if ($pageVars["route"]["action"] == "login") {
-            $this->content["data"] = $thisModel->getlogin();
-        }
+            $this->content["data"] = $thisModel->getlogin(); }
 
         if ($pageVars["route"]["action"] == "registration") {
             // todo @karthik do we need the line below
             // $this->content["data"] = "registration";
-            return array("type" => "view", "view" => "signupRegistration", "pageVars" => $this->content);
-        }
+            return array("type" => "view", "view" => "signupRegistration", "pageVars" => $this->content); }
 
         if ($pageVars["route"]["action"] == "registration-submit") {
             $this->content["data"] = $thisModel->registrationSubmit();
             $this->content["output-format"] = "JSON" ;
-            return array("type" => "view", "view" => "signupRegistrationResult", "pageVars" => $this->content);
-        }
+            return array("type" => "view", "view" => "signupRegistrationResult", "pageVars" => $this->content); }
 
         if ($pageVars["route"]["action"] == "login-status") {
             $this->content["data"] =  $thisModel->checkLoginStatus();
@@ -44,12 +38,10 @@ class Signup extends Base
             return array("type" => "view", "view" => "signupLoginSubmit", "pageVars" => $this->content); }
 
         if ($pageVars["route"]["action"] == "logout") {
-            $thisModel->allLoginInfoDestroy();
-        }
+            $thisModel->allLoginInfoDestroy(); }
 		
 		if ($pageVars["route"]["action"] == "verify") {
-            $thisModel->mailVerification();
-        }
+            $thisModel->mailVerification(); }
 		
         return array("type" => "view", "view" => "signupLogin", "pageVars" => $this->content);
 
