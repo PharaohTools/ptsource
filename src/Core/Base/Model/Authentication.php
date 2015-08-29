432ff2c34deac2bd20f2808c0fa5750e9bf2bfeb
David Amanshia <phpengine@pharaohtools.com>
2015-08-29 22:20:40 +0100

    added ignored authentication routes to authentication

--- src/Core/Base/Model/Authentication.php
+++ src/Core/Base/Model/Authentication.php
@@ -17,8 +17,23 @@ class Authentication {
         $handled["pageVars"] = $pageVars ;
         if (!isset($mod_config["Signup"]["signup_enabled"]) || $mod_config["Signup"]["signup_enabled"]=="off") {
             return $handled  ; }
+
+
+        // find modules which provide ignore auth routes
+        $ignoredAuthRoutes = array();
+        $infos = \Core\AutoLoader::getInfoObjects();
+
+        foreach ($infos as $info) {
+            if (method_exists($info, "ignoredAuthenticationRoutes")) {
+                $ignoredAuthRoutes[$info->getModuleName()] = $info->ignoredAuthenticationRoutes() ; } }
+        if (array_key_exists($handled["control"], $ignoredAuthRoutes) &&
+            in_array($handled["action"], $ignoredAuthRoutes[$handled["control"]])) {
+            // if we are requesting something with ignored auth, just return it
+            error_log("ignoring auth for {$handled["control"]}, {$handled["action"]} ") ;
+            return $handled ; }
+
         // if we have failed authentication, we will have a false
-//        var_dump($ev) ;
+        // var_dump($ev) ;
         if (in_array(false, $ev)) {
             error_log("failed authentication, forcing back to signup module") ;
             $handled["control"] = "Signup";
@@ -34,7 +49,7 @@ class Authentication {
         $handled["pageVars"] = $pageVars ;
         $umf = new \Model\UserManager();
         $um = $umf->getModel(array());
-        // the user is logged in, but resticted
+        // the user is logged in, but restricted
         $restricted = $um->getRestrictionStatus() ;
         if ($restricted === true) {
             error_log("restricted user authentication, forcing back to signup module") ;
