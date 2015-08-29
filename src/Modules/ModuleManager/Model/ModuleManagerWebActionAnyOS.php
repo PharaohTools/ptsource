bca0ece0110e02d15be9070c04e7214f7e2b0d52
David Amanshia <phpengine@pharaohtools.com>
2015-08-29 23:47:06 +0100

    web actions for module manager

--- src/Modules/ModuleManager/Model/ModuleManagerWebActionAnyOS.php
+++ src/Modules/ModuleManager/Model/ModuleManagerWebActionAnyOS.php
@@ -15,19 +15,44 @@ class ModuleManagerWebActionAnyOS extends BasePHPApp {
     public $modelGroup = array("WebAction") ;
 
     public function getData() {
-        $ret["mod_install"] = $this->installModules();
+        $ret["mod_installed"] = $this->installModules();
+        $ret["mod_enabled"] = $this->enableModule();
+        $ret["mod_disabled"] = $this->disableModule();
         return $ret ;
     }
 
-    private function installModules() {
+    public function installModules() {
         //var_dump($_REQUEST["module-source"]);
         if (isset($_REQUEST["module-source"]) && strlen($_REQUEST["module-source"]) > 0) {
             $modFactory = new \Model\ModuleManager() ;
             $mmpr = $this->params ;
             $mmpr["module-source"] = $_REQUEST["module-source"] ;
             $mm = $modFactory->getModel($mmpr);
-            $mm->install(); }
-        return true ;
+            $mm->install();
+            return array("installed-status" => true, "module-install" => $this->params["module-install"]) ; }
+        return array() ;
+    }
+
+    public function enableModule() {
+        if (isset($_REQUEST["module-enable"]) && strlen($_REQUEST["module-enable"]) > 0) {
+            $modFactory = new \Model\ModuleManager() ;
+            $mmpr = $this->params ;
+            $mmpr["module-enable"] = $_REQUEST["module-enable"] ;
+            $mm = $modFactory->getModel($mmpr);
+            $mm->enableModule();
+            return array("enabled-status" => true, "module-enable" => $this->params["module-enable"]) ; }
+        return array() ;
+    }
+
+    public function disableModule() {
+        if (isset($_REQUEST["module-disable"]) && strlen($_REQUEST["module-disable"]) > 0) {
+            $modFactory = new \Model\ModuleManager() ;
+            $mmpr = $this->params ;
+            $mmpr["module-disable"] = $_REQUEST["module-disable"] ;
+            $mm = $modFactory->getModel($mmpr);
+            $mm->disableModule();
+            return array("disabled-status" => true, "module-disable" => $this->params["module-disable"]) ; }
+        return array() ;
     }
 
 }
