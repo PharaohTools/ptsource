89d9dac5047c0cc0105fd659129136a3a7fdee19
David Amanshia <phpengine@pharaohtools.com>
2015-08-29 17:08:01 +0100

    auth event has been moved to auth model

--- src/Core/Control.php
+++ src/Core/Control.php
@@ -5,12 +5,8 @@ Namespace Core ;
 class Control {
 
     public function executeControl($control, $pageVars) {
-        $eventRunnerFactory = new \Model\EventRunner() ;
-        $eventRunner = $eventRunnerFactory->getModel($pageVars) ;
-        $ev = $eventRunner->eventRunner("authenticate", true) ;
-        // @todo lets get the below from a model, overridable
         $auth = new \Model\Authentication() ;
-        $handled = $auth->handleAuthenticationResponse($ev, $control, $pageVars) ;
+        $handled = $auth->handleAuthenticationResponse($control, $pageVars) ;
         $className = '\\Controller\\'.ucfirst($handled["control"]);
         $controlObject = new $className;
         return $controlObject->execute($handled["pageVars"]);
