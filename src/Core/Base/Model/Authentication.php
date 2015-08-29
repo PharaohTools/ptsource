bf34c2d3ed7ba5767b69bb826c4b4aa9f1b1a0d0
David Amanshia <phpengine@pharaohtools.com>
2015-08-29 17:07:36 +0100

    auth model runs the auth event

--- src/Core/Base/Model/Authentication.php
+++ src/Core/Base/Model/Authentication.php
@@ -5,7 +5,12 @@ Namespace Model;
 class Authentication {
 
     // @todo everything below this line should probably be ion a model, or at least a helper
-    public function handleAuthenticationResponse($ev, $control, $pageVars) {
+    public function handleAuthenticationResponse($control, $pageVars) {
+
+        $eventRunnerFactory = new \Model\EventRunner() ;
+        $eventRunner = $eventRunnerFactory->getModel($pageVars) ;
+        $ev = $eventRunner->eventRunner("authenticate", true) ;
+
         $mod_config = \Model\AppConfig::getAppVariable("mod_config");
         $handled = array() ;
         $handled["control"] = $control ;
@@ -20,11 +25,11 @@ class Authentication {
             $handled["pageVars"]["route"]["control"] = "Signup" ;
             return $handled ; }
         // at this point we are authenticated, so lets check authorization too
-        $handled = $this->handleAuthorizationResponse($ev, $handled["control"], $handled["pageVars"]) ;
+        $handled = $this->handleAuthorizationResponse($handled["control"], $handled["pageVars"]) ;
         return $handled  ;
     }
 
-    private function handleAuthorizationResponse($ev, $control, $pageVars) {
+    private function handleAuthorizationResponse($control, $pageVars) {
         $handled = array() ;
         $handled["pageVars"] = $pageVars ;
         $umf = new \Model\UserManager();
@@ -36,16 +41,20 @@ class Authentication {
             $handled["control"] = "Signup";
             $handled["pageVars"]["route"]["control"] = "Signup" ;
             return $handled ; }
-        $myRole = $um->getMyUserRoleId() ;
+        // @todo this is not needed for this app
+        // @todo this looks like it should be supported in module code
+        // a module should be able to define
+//        $myRole = $um->getMyUserRoleId() ;
         // if we are a client use client
-        if ($myRole==4) {
-            $clientAllowedList = array(
-                "ClientPortal", "ClientJobList", "ClientJobHome", "ClientDocuments", "PostInput",
-                "AssetLoader", "Signup", "About" ) ;
-            if (!in_array($control, $clientAllowedList)) {
-                $handled["control"] = "ClientPortal";
-                $handled["pageVars"]["route"]["control"] = "ClientPortal" ;
-                return $handled ; } }
+
+//        if ($myRole==4) {
+//            $clientAllowedList = array(
+//                "ClientPortal", "ClientJobList", "ClientJobHome", "ClientDocuments", "PostInput",
+//                "AssetLoader", "Signup", "About" ) ;
+//            if (!in_array($control, $clientAllowedList)) {
+//                $handled["control"] = "ClientPortal";
+//                $handled["pageVars"]["route"]["control"] = "ClientPortal" ;
+//                return $handled ; } }
         $handled["control"] = $control;
         $handled["pageVars"]["route"]["control"] = $control ;
         return $handled ;
