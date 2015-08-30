6f8557282b46a550ea67dfedd9a189bd914ecb37
David Amanshia <phpengine@pharaohtools.com>
2015-08-30 02:16:30 +0100

    build list js started getting ready to update finished build rows with new results

--- src/Modules/BuildList/Assets/js/buildlist.js
+++ src/Modules/BuildList/Assets/js/buildlist.js
@@ -30,7 +30,6 @@ function setRunningBuildList(data) {
     data = JSON.parse(data);
     console.log(data);
     if (data.length == 0) {
-        
         //$('#blRow_'+data[index].item +" > th ").html(row);
           $('.runningBuildRow' +" > td ").animate({ opacity: 100 });
           $('.runningBuildRow' +" > th ").animate({ opacity: 100 });
@@ -47,7 +46,7 @@ function setRunningBuildList(data) {
             //row = $('#blRow_'+data[index].item +" > td ").html();
             //$('#blRow_'+data[index].item +" > td ").html(' ');
             ht += '<div class=" well well-sm">' ;
-            ht += '<img src="Assets/startbootstrap-sb-admin-2-1.0.5/dist/image/rt.GIF" style="width:150px;">' ;
+            ht += '  <img src="Assets/startbootstrap-sb-admin-2-1.0.5/dist/image/rt.GIF" style="width:150px;">' ;
             ht += '  <h5><strong>Pipeline:</strong> '+data[index].item+'</h5>' ;
             ht += '  <h5><strong>Build start at:</strong> '+data[index].starttime+'</h5>' ;
             ht += '  <h5><strong>Pipedir:</strong> '+data[index].pipedir+'</h5>' ;
