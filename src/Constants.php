d44f00312d076be271ee05064f765415fba9711f
David Amanshia <phpengine@pharaohtools.com>
2015-08-30 23:02:00 +0100

    new constants for updated app names

--- src/Constants.php
+++ src/Constants.php
@@ -13,30 +13,35 @@ if (in_array(PHP_OS, array("Windows", "WINNT"))) {
     $command = "where /R \"{$pf}\" *VBoxManage* " ;
     $outputArray = array();
     exec($command, $outputArray);
+    define('SUDOPREFIX', "");
     define('VBOXMGCOMM', "\"{$outputArray[0]}\" ") ;
     define('PFILESDIR', $sd."\\PharaohTools\\") ;
     define('PTCCOMM', PFILESDIR.'ptconfigure.cmd"') ;
     define('PTDCOMM',  PFILESDIR."ptdeploy.cmd") ;
-    define('PHLCOMM',  PFILESDIR."phlagrant.cmd") ;
-    define('PHRCOMM',  PFILESDIR."ptbuild.cmd") ;
-    define('HORCOMM',  PFILESDIR."horus.cmd") ;
+    define('PTVCOMM',  PFILESDIR."ptvirtualize.cmd") ;
+    define('PTBCOMM',  PFILESDIR."ptbuild.cmd") ;
+    define('PTTRCOMM',  PFILESDIR."pttrack.cmd") ;
     define('RACOMM',  PFILESDIR."ra.cmd") ;
     define('BOXDIR', $sd.'\\PharaohTools\boxes') ;
     define('PIPEDIR', $sd.'\\PharaohTools\pipes'.'\\') ;
-	define('PLUGININS', $sd.'\\PharaohTools\plugins/installed'.'\\') ;
+	define('PLUGININS', $sd.'\\PharaohTools\plugins\installed'.'\\') ;
     define("DS", "\\");
     define("BASE_TEMP_DIR", getenv("SystemDrive").'\Temp\\'); }
 else if (in_array(PHP_OS, array("Linux", "Solaris", "FreeBSD", "OpenBSD", "Darwin"))) {
     define('DS', "/") ;
+    $uname = exec('whoami');
+    $isAdmin = ($uname == "root") ? true : false ;
+    if ($isAdmin == true) { define('SUDOPREFIX', ""); }
+    else { define('SUDOPREFIX', "sudo "); }
     define('VBOXMGCOMM', "vboxmanage ") ;
     define('PFILESDIR', "/opt/") ;
     define('PTCCOMM', "ptconfigure ") ;
     define('PTDCOMM', "ptdeploy ") ;
-    define('PHLCOMM', "phlagrant") ;
-    define('PHRCOMM', "ptbuild") ;
-    define('HORCOMM', "horus") ;
+    define('PTVCOMM', "ptvirtualize") ;
+    define('PTBCOMM', "ptbuild") ;
+    define('PTTRCOMM', "pttrack") ;
     define('RACOMM', "ra") ;
     define("BASE_TEMP_DIR", '/tmp/');
-    define('BOXDIR', '/opt/phlagrant/boxes') ;
+    define('BOXDIR', '/opt/ptvirtualize/boxes') ;
     define('PIPEDIR', '/opt/ptbuild/pipes') ;
     define('PLUGININS', '/opt/ptbuild/plugins/installed') ; }
