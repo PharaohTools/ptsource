8961e0ef7e6c17021a7e22d1ec5ab55fb59c1b17
David Amanshia <phpengine@pharaohtools.com>
2015-08-31 02:30:54 +0100

    pipe runner web termination started

---
+++ src/Modules/PipeRunner/Assets/js/pipeterminator.js
@@ -0,0 +1,64 @@
+done = false ;
+max = 0 ;
+window.updateRunning = false ;
+window.outUpdater = setInterval(function () {
+    if (window.updateRunning==false) {
+        console.log("calling update page js method, updateRunning variable is set to false");
+        updatePage() ; }
+    else {
+        console.log("not calling update page js method, updateRunning variable is set to true"); }
+}, 4000);
+
+function updatePage() {
+    console.log("running update page js method");
+    window.updateRunning = true ;
+    console.log("setting update running to true");
+    item = window.pipeitem ;
+    url = "/index.php?control=PipeRunner&action=service&item=" + item + "&output-format=SERVICE";
+    $.ajax({
+        url: url,
+        success: function(data) {
+            $('#updatable').html(data);
+            window.updateRunning = false ; },
+        complete: function() {
+            // Schedule the next request when the current one's complete
+            setStatus();
+            console.log("Req Status: " + window.reqStatus);
+            if (window.reqStatus == "OK") {
+                doCompletion(); }
+            window.updateRunning = false ; }
+    });
+}
+
+function setStatus() {
+    item = window.pipeitem ;
+    pid = window.runid ;
+    url = "/index.php?control=PipeRunner&action=pipestatus&item=" + item + "&pid=" + pid + "&output-format=PIPESTATUS";
+    console.log(url);
+    $.ajax({
+        url: url,
+        success: function(data) {
+            window.reqStatus = data ;
+            console.log("pipestat: " + data) ;
+        }
+    });
+}
+
+function doCompletion() {
+    removeWaitImage();
+    changeSubButton();
+    clearInterval(window.outUpdater);
+}
+
+function removeWaitImage() {
+    $("#loading-holder").hide() ;
+}
+
+function changeSubButton() {
+    subhtml  = '<div class="col-sm-offset-2 col-sm-8">';
+    subhtml += '  <div class="text-center">';
+    subhtml += '    <button type="submit" class="btn btn-primary" id="close-complete">Close Execution Screen</button>';
+    subhtml += '  </div>';
+    subhtml += '</div>' ;
+    $("#submit-holder").html(subhtml) ;
+}
