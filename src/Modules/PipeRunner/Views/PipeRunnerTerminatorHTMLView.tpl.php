bbcaef54d85b7cd0e5303a379344fbc0e57e8a3b
David Amanshia <phpengine@pharaohtools.com>
2015-08-31 02:33:32 +0100

    work on terminator html view

--- src/Modules/PipeRunner/Views/PipeRunnerTerminatorHTMLView.tpl.php
+++ src/Modules/PipeRunner/Views/PipeRunnerTerminatorHTMLView.tpl.php
@@ -59,35 +59,10 @@
                     <div class="well well-lg">
             <h2 class="text-uppercase text-light"><a href="/"> PTBuild - Pharaoh Tools </a></h2>
             <div class="row clearfix no-margin">
-                <?php
-                    switch ($pageVars["route"]["action"]) {
-                        case "start" :
-                            $stat = "Now Executing " ;
-                            break ;
-                        case "history" :
-                            $stat = "Historic Builds of " ;
-                            break ;
-                        case "summary" :
-                            $stat = "Execution Summary of " ;
-                            break ; }
-                ?>
-                <h3><?= $stat; ?> Pipeline <?php echo $pageVars["data"]["pipeline"]["project-name"] ; ?>
 
-                    <?php
-                    if ($pageVars["route"]["action"] == "summary") {
-                        echo ', Run '.$pageVars["data"]["historic_build"]["run-id"] ; }
-                    ?>
-
-                    <i style="font-size: 18px;" </i></h3>
-                <h5 class="text-uppercase text-light" style="margin-top: 15px;">
-                    <a href="/index.php?control=BuildHome&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"></a>
-                </h5>
-                <?php
-                    if ($pageVars["route"]["action"] != "summary") {
-                        $act = '/index.php?control=PipeRunner&item='.$pageVars["data"]["pipeline"]["project-slug"].'&action=summary' ; }
-                    else {
-                        $act = '/index.php?control=PipeRunner&item='.$pageVars["data"]["pipeline"]["project-slug"].'&action=summary&run-id='.$pageVars["data"]["historic_build"]["run-id"]  ; }
-                ?>
+                <h3>Now Terminating Pipeline <?php echo $pageVars["data"]["pipeline"]["project-name"] ; ?><?php
+                        echo ', Run '.$pageVars["data"]["historic_build"]["run-id"] ;
+                    ?> </h3>
 
                 <form class="form-horizontal custom-form" action="<?= $act ; ?>" method="POST">
 
@@ -97,7 +72,7 @@
 
                         <div class="form-group">
                             <div class="col-sm-10">
-                                Pipeline Execution started - Run # <?= $pageVars["pipex"] ;?>
+                                Pipeline Termination started - Run # <?= $pageVars["pipex"] ;?>
                             </div>
                         </div>
 
@@ -108,81 +83,59 @@
                     <div class="form-group">
                         <div class="col-sm-12">
                             <div id="updatable">
-                                Checking Pipeline Execution Output...
-                                <?php
+                                Checking Pipeline Termination Output...
 
-                                if ($pageVars["route"]["action"]=="history") {
-                                    echo '<p>Historic builds</p>';
-
-                                    foreach ($pageVars["data"]["historic_builds"] as $hb) {
-                                        echo '<a href="/index.php?control=PipeRunner&action=summary&item='.$pageVars["data"]["pipeline"]["project-slug"].'&run-id='.$hb.'">'.$hb.'</a><br />' ; } }
-                                else if ($pageVars["route"]["action"]=="summary") {
-                                    echo '<pre>'.$pageVars["data"]["historic_build"]["out"].'</pre>'; }
-                                ?>
                             </div>
                         </div>
                     </div>
-                    <?php
-                    if ($pageVars["route"]["action"] =="start") {
-                        echo '
-                          <script type="text/javascript">
-                              window.pipeitem = "'.$pageVars["data"]["pipeline"]["project-slug"].'" ;
-                              window.runid = "'.$pageVars["pipex"].'" ;
-                          </script>
-                              <script type="text/javascript" src="/Assets/Modules/PipeRunner/js/piperunner.js"></script>
-                              <div class="form-group" id="loading-holder">
-                                  <div class="col-sm-offset-2 col-sm-8">
-                                      <div class="text-center  ">
-                                          
-                                      </div>
-                                 </div>
-                             </div>'; }
-                    ?>
 
+                      <script type="text/javascript">
+                          window.pipeitem = "<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>" ;
+                          window.runid = "<?php echo $pageVars["data"]["historic_build"]["run-id"] ; ?>" ;
+                      </script>
+                          <div class="form-group" id="loading-holder">
+                              <div class="col-sm-offset-2 col-sm-8">
+                                  <div class="text-center  ">
 
+                                  </div>
+                             </div>
+                      </div>
 
-                    <?php
-                    if ($pageVars["route"]["action"] =="start") {
-                    ?>
-                        <div class="form-group" id="submit-holder">
-                            <div class="col-sm-offset-2 col-sm-8">
-                                <div class="text-center">
-                                    <img src="Assets/startbootstrap-sb-admin-2-1.0.5/dist/image/712.GIF" style="width:100px;">
-                                </div>
+                    <div class="form-group" id="submit-holder">
+                        <div class="col-sm-offset-2 col-sm-8">
+                            <div class="text-center">
+                                <img src="Assets/startbootstrap-sb-admin-2-1.0.5/dist/image/712.GIF" style="width:100px;">
                             </div>
                         </div>
-                        <div class="form-group" id="submit-holder">
-                            <div class="col-sm-offset-2 col-sm-8">
-                                <div class="text-center">
-                                    <?php
-                                    $termLink = '/index.php?control=PipeRunner&action=terminate&run-id='.$pageVars["pipex"].'&item='.$pageVars["data"]["pipeline"]["project-slug"] ;
-                                    ?>
-                                    <a href="<?php echo $termLink ; ?>" type="submit" class="btn btn-danger hvr-float-shadow" id="terminate-build">
-                                        Terminate Build
-                                    </a>
-                                </div>
-
+                    </div>
+                    <div class="form-group" id="submit-holder">
+                        <div class="col-sm-offset-2 col-sm-8">
+                            <div class="text-center">
+                                <?php
+                                $termLink = '/index.php?control=PipeRunner&action=terminate&run-id='.$pageVars["pipex"].'&item='.$pageVars["data"]["pipeline"]["project-slug"] ;
+                                ?>
+                                <a href="<?php echo $termLink ; ?>" type="submit" class="btn btn-danger hvr-float-shadow" id="terminate-build">
+                                    Terminate Build
+                                </a>
                             </div>
+
                         </div>
-                        <div class="form-group" id="submit-holder">
-                            <div class="col-sm-offset-2 col-sm-8">
-                                <div class="text-center">
+                    </div>
+                    <div class="form-group" id="submit-holder">
+                        <div class="col-sm-offset-2 col-sm-8">
+                            <div class="text-center">
 
 
-                                    <button type="submit" class="btn btn-danger hvr-float-shadow" id="end-now">End Now</button>
-                                </div>
-
+                                <button type="submit" class="btn btn-danger hvr-float-shadow" id="end-now">End Now</button>
                             </div>
+
                         </div>
-                    <?php
-                       }
-                    ?>
+                    </div>
 
                     <input type="hidden" id="item" value="<?= $pageVars["data"]["pipeline"]["project-slug"] ;?>" />
                     <input type="hidden" id="pid" value="<?= $pageVars["pipex"] ;?>" />
                     <?php
-                    if ($pageVars["route"]["action"] == "summary") {
-                        echo '<input type="hidden" id="run-id" value="'.$pageVars["data"]["historic_build"]["run-id"].'" />' ; }
+                        echo '<input type="hidden" id="run-id" value="'.$pageVars["data"]["historic_build"]["run-id"].'" />' ;
                     ?>
 
                 </form>
@@ -196,3 +149,5 @@
 
     </div>
 </div><!-- /.container -->
+
+<script type="text/javascript" src="/Assets/Modules/PipeRunner/js/pipeterminator.js"></script>
