a3bce199d75bba55963ff6803e333a1c3f6bbf5f
David Amanshia <phpengine@pharaohtools.com>
2015-09-09 05:13:04 +0100

    fixed docker output it was runnign when it shouldnt be

--- src/Modules/Docker/Model/DockerLinuxUnix.php
+++ src/Modules/Docker/Model/DockerLinuxUnix.php
@@ -54,31 +54,32 @@ class DockerLinuxUnix extends Base {
 		$this->params["echo-log"] = true;
 		$logging = $loggingFactory->getModel($this->params);
 		$mn = $this->getModuleName();
-		if (isset($pipeline["settings"][$mn]["docker"]) && $this->params["build-settings"][$mn]["docker"] != "on"){
+		if (!isset($pipeline["settings"][$mn]["docker"]) ||
+            (isset($pipeline["settings"][$mn]["docker"]) && $this->params["build-settings"][$mn]["docker"] != "on") ){
 //			$logging->log("Docker Not enabled for build, ignoring...");
 			return TRUE;
 		}
 		if (!$this->ensureInstalled() == false)
 		{
-			$logging->log("Docker Not installed", $this->getModuleName());
+//			$logging->log("Docker Not installed", $this->getModuleName());
 			return false;
 		}
 		else {
 			$logging->log("Running Docker ".$this->params["build-settings"][$mn]["repository"]. ' container');
-			if ($this->executeAndGetReturnCode('docker start '.$this->params["build-settings"][$mn]["repository"].' &') > 0) {
+			 if ($this->executeAndGetReturnCode('docker start '.$this->params["build-settings"][$mn]["repository"].' &') > 0) {
 				$logging->log("Pulling image from Docker repo", $this->getModuleName());
 				$this->executeAndOutput('docker pull '.$this->params["build-settings"][$mn]["repository"]); }
 			$this->executeAndGetReturnCode('docker start '.$this->params["build-settings"][$mn]["repository"].' &');
 			sleep(10);
 			$container_id = $this->executeAndLoad("docker ps | grep ".$this->params["build-settings"][$mn]["repository"]." | awk '{print $1}'");
 			echo $container_id;
-//			$logging->log("Running command inside ".$this->params["build-settings"][$mn]["repository"]. ' container');
+			$logging->log("Running command inside ".$this->params["build-settings"][$mn]["repository"]. ' container');
 			$container_id = str_replace(PHP_EOL, '', $container_id);
 			$this->executeAndGetReturnCode('docker exec '.$container_id.' '.$this->params["build-settings"][$mn]["commandToRun"]);
 			
 			$logging->log('Stopping container');
-			//$this->executeAndGetReturnCode('docker stop '.$container_id);
-			return TRUE;
+			$rc = $this->executeAndGetReturnCode('docker stop '.$container_id);
+			return ($rc["rc"]==0) ? true : false ;
 		}
 	}
 	
