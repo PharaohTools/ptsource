f0831afc483e5dedb7f4d2d8b36e07fa010a4248
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-11-24 22:38:52 +0000

    build ome calls single build execution check correctly

--- src/Modules/BuildHome/Assets/js/buildhome.js
+++ src/Modules/BuildHome/Assets/js/buildhome.js
@@ -13,8 +13,8 @@ function updatePage() {
     console.log("running update page js method");
     window.updateRunning = true ;
     console.log("setting update running to true");
-    item = window.pipeitem ;
-    url = "/index.php?control=PipeRunner&action=findrunning&item="+item+"&output-format=JSON";
+    item = getQueryParam("item") ;
+    url = "/index.php?control=PipeRunner&action=findrunning&pipeline="+item+"&output-format=JSON";
     $.ajax({
         url: url,
         success: function(data) {
@@ -57,4 +57,13 @@ function setRunningBuildList(data) {
             ht += '</div>' ;}
             
         $('#runningBuilds').html(ht); }
+}
+
+function getQueryParam(param) {
+    location.search.substr(1)
+        .split("&")
+        .some(function(item) { // returns first occurence and stops
+            return item.split("=")[0] == param && (param = item.split("=")[1])
+        })
+    return param
 }
