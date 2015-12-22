f4bffc6f2135eaeef1f1a040f7989ab857b19cd2
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-22 12:00:40 +0000

    ifconfig path for osx fixed

--- src/Modules/SystemDetection/Model/SystemDetectionAllOS.php
+++ src/Modules/SystemDetection/Model/SystemDetectionAllOS.php
@@ -149,21 +149,26 @@ class SystemDetectionAllOS extends Base {
                 // @todo surely captain, there must be a better way
                 exec('yum install net-tools -y', $outputArray);
             }
-            $ifComm = 'sudo ip addr list | awk \'/inet /{sub(/\/[0-9]+/,"",$2); print $2}\' ';
+            $ifComm = SUDOPREFIX.' ip addr list | awk \'/inet /{sub(/\/[0-9]+/,"",$2); print $2}\' ';
             // $ifComm = "sudo ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'" ;
             exec($ifComm, $outputArray);
             foreach($outputArray as $outputLine ) {
                 $this->ipAddresses[] = $outputLine ; } }
         else if ($this->os == "Solaris") {
-            $ifComm = "sudo ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'" ;
+            $ifComm = SUDOPREFIX." ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'" ;
             exec($ifComm, $outputArray);
             foreach($outputArray as $outputLine ) {
                 $this->ipAddresses[] = $outputLine ; } }
-        else if (in_array($this->os, array("FreeBSD", "OpenBSD", "Darwin"))) {
-            $ifComm = "sudo ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'" ;
+        else if (in_array($this->os, array("FreeBSD", "OpenBSD"))) {
+            $ifComm = SUDOPREFIX." ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'" ;
             exec($ifComm, $outputArray);
             foreach($outputArray as $outputLine ) {
                 $this->ipAddresses[] = $outputLine ; } }
+        else if (in_array($this->os, array("Darwin"))) {
+                $ifComm = SUDOPREFIX." /sbin/ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'" ;
+                exec($ifComm, $outputArray);
+                foreach($outputArray as $outputLine ) {
+                    $this->ipAddresses[] = $outputLine ; } }
     }
 
 }
