4494c48e449b70282bf397dda25446768f3be1d1
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-25 23:54:55 +0000

    only updating page when its active

--- src/Modules/BuildList/Assets/js/buildlist.js
+++ src/Modules/BuildList/Assets/js/buildlist.js
@@ -4,27 +4,26 @@ window.updateRunning = false ;
 window.outUpdater = setInterval(function () {
     if (window.updateRunning==false) {
         console.log("calling update page js method, updateRunning variable is set to false");
-        updatePage() ; }
+        if (document.hasFocus()!==false) {
+            updatePage() ; } }
     else {
         console.log("not calling update page js method, updateRunning variable is set to true"); }
 }, 4000);
 
 function updatePage() {
     console.log("running update page js method");
-    if (document.hasFocus()==true) {
-        window.updateRunning = true ;
-        console.log("setting update running to true");
-        item = window.pipeitem ;
-        url = "/index.php?control=PipeRunner&action=findrunning&output-format=JSON";
-        $.ajax({
-            url: url,
-            success: function(data) {
-                setRunningBuildList(data) ;
-                window.updateRunning = false ; } ,
-            complete: function(data) {
-                window.updateRunning = false ; }
-        });
-    }
+    window.updateRunning = true ;
+    console.log("setting update running to true");
+    item = window.pipeitem ;
+    url = "/index.php?control=PipeRunner&action=findrunning&output-format=JSON";
+    $.ajax({
+        url: url,
+        success: function(data) {
+            setRunningBuildList(data) ;
+            window.updateRunning = false ; } ,
+        complete: function(data) {
+            window.updateRunning = false ; }
+    });
 }
 
 var row;
