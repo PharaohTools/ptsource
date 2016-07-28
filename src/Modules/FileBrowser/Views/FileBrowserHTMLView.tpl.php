7970ccdef5ca76f58fb4ed42006a39a501709fdb
David Amanshia <phpengine@pharaohtools.com>
2016-07-28 03:50:41 +0100

    file browser labels

--- src/Modules/FileBrowser/Views/FileBrowserHTMLView.tpl.php
+++ src/Modules/FileBrowser/Views/FileBrowserHTMLView.tpl.php
@@ -19,34 +19,34 @@
                 </li>
                 <li>
                     <a href="index.php?control=BuildList&action=show"class="hvr-bounce-in">
-                        <i class="fa fa-home hvr-bounce-in"></i>  Pipeline Home
+                        <i class="fa fa-home hvr-bounce-in"></i>  Repository Home
                     </a>
                 </li>
                 <li>
                     <a href="/index.php?control=BuildList&amp;action=show"class="hvr-bounce-in">
-                        <i class="fa fa-bars hvr-bounce-in"></i> All Pipelines
+                        <i class="fa fa-bars hvr-bounce-in"></i> All Repositories
                     </a>
                 </li>
                 
                 
                 <li>
-                    <a href="index.php?control=FileBrowser&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"class="hvr-bounce-in">
-                        <i class="fa fa-folder-open-o hvr-bounce-in"></i> FileBrowser
+                    <a href="index.php?control=FileBrowser&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"] ; ?>"class="hvr-bounce-in">
+                        <i class="fa fa-folder-open-o hvr-bounce-in"></i> File Browser
                     </a>
                 </li>
                 <li>
-                    <a href="index.php?control=BuildMonitor&action=show&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"class="hvr-bounce-in">
+                    <a href="index.php?control=BuildMonitor&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"] ; ?>"class="hvr-bounce-in">
                         <i class="fa fa-bar-chart-o hvr-bounce-in"></i> Monitors
                     </a>
                 </li>
                 <li>
-                    <a href="index.php?control=PipeRunner&action=history&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"class="hvr-bounce-in">
+                    <a href="index.php?control=PipeRunner&action=history&item=<?php echo $pageVars["data"]["repository"]["project-slug"] ; ?>"class="hvr-bounce-in">
                         <i class="fa fa-history hvr-bounce-in"></i> History <span class="badge"></span>
                     </a>
                 </li>
                 
                 <li>
-                    <a href="/index.php?control=FileBrowser&action=start&item=<?php echo $pageVars["data"]["pipeline"]["project-slug"] ; ?>"class="hvr-bounce-in">
+                    <a href="/index.php?control=FileBrowser&action=start&item=<?php echo $pageVars["data"]["repository"]["project-slug"] ; ?>"class="hvr-bounce-in">
                         <i class="fa fa-sign-in fa-fw hvr-bounce-in"></i> Run Again
                     </a>
                 </li>
@@ -66,16 +66,16 @@
                 <?php
                     switch ($pageVars["route"]["action"]) {
                         case "show" :
-                            $stat = "FileBrowser From " ;
+                            $stat = "Browsing Files From " ;
                             break ; }
                 ?>
-                <h3><?= $stat; ?> Pipeline <?php echo $pageVars["data"]["pipeline"]["project-name"] ; ?></h3>
+                <h3><?= $stat; ?> Repository <?php echo $pageVars["data"]["repository"]["project-name"] ; ?></h3>
                 <?php
                     $rootPath = str_replace($pageVars["data"]["relpath"], "", $pageVars["data"]["wsdir"]) ;
                     echo '<h3><a href="/index.php?control=FileBrowser&action=show&item='.
-                         $pageVars["data"]["pipeline"]["project-slug"].'">'.$rootPath.'</a></h3>' ;
+                         $pageVars["data"]["repository"]["project-slug"].'">'.$rootPath.'</a></h3>' ;
 
-                    $act = '/index.php?control=FileBrowser&item='.$pageVars["data"]["pipeline"]["project-slug"].'&action=show' ;
+                    $act = '/index.php?control=FileBrowser&item='.$pageVars["data"]["repository"]["project-slug"].'&action=show' ;
                 ?>
 
                 <form class="form-horizontal custom-form" action="<?= $act ; ?>" method="POST">
@@ -89,13 +89,13 @@
 
                                         $dirString = ($isDir) ? " - (D)" : "" ;
                                         $trail = ($isDir) ? "/" : "" ;
-                                        echo '<a href="/index.php?control=FileBrowser&action=show&item='.$pageVars["data"]["pipeline"]["project-slug"].'&relpath='.$pageVars["data"]["relpath"].'">'.$pageVars["data"]["relpath"].'</a>' ;
+                                        echo '<a href="/index.php?control=FileBrowser&action=show&item='.$pageVars["data"]["repository"]["project-slug"].'&relpath='.$pageVars["data"]["relpath"].'">'.$pageVars["data"]["relpath"].'</a>' ;
 
                                         $relativeString = str_replace($pageVars["data"]["wsdir"], "", $name) ;
                                         $nameparts = explode(DS, $relativeString) ;
 
                                         foreach ($nameparts as $namepart => $isSubDir) {
-                                            echo '<a href="/index.php?control=FileBrowser&action=show&item='.$pageVars["data"]["pipeline"]["project-slug"].'&relpath='.$pageVars["data"]["relpath"].$name.
+                                            echo '<a href="/index.php?control=FileBrowser&action=show&item='.$pageVars["data"]["repository"]["project-slug"].'&relpath='.$pageVars["data"]["relpath"].$name.
                                                 $trail.'">'.$name.'</a>' ; }
 
                                         echo $trail.$dirString.'<br />' ; } }
