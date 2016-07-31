1a855c29ad1b7a71e71b1bf2e369937ffae3a549
David Amanshia <phpengine@pharaohtools.com>
2016-08-01 00:33:09 +0100

    repos are collated with their features

--- src/Modules/Repository/Model/RepositoryCollaterAllOS.php
+++ src/Modules/Repository/Model/RepositoryCollaterAllOS.php
@@ -24,6 +24,7 @@ class RepositoryCollaterAllOS extends Base {
         $collated = array() ;
         $collated = array_merge($collated, $this->getDefaults()) ;
         $collated = array_merge($collated, $this->getSettings()) ;
+        $collated = array_merge($collated, $this->getFeatures()) ;
         return $collated ;
     }
 
@@ -65,6 +66,48 @@ class RepositoryCollaterAllOS extends Base {
             $logging = $loggingFactory->getModel($this->params) ;
             $logging->log("No settings file available in repository ".$this->params["item"], $this->getModuleName()) ; }
         return array("settings" => $settings) ;
+    }
+
+    private function getFeatures() {
+
+        $repositoryFeatureFactory = new \Model\RepositoryFeature();
+        $repositoryFeatureRepository = $repositoryFeatureFactory->getModel($this->params, "RepositoryFeatureRepository") ;
+        $names = $repositoryFeatureRepository->getRepositoryFeatureNames();
+        $repositorySettings = $this->getSettings();
+        $repositoryDefaults = $this->getDefaults();
+        $this->params["repository-settings"] = $repositorySettings["settings"];
+        $repository = array() ;
+        $repository = array_merge($repository, $repositorySettings);
+        $repository = array_merge($repository, $repositoryDefaults);
+        $enabledFeatures = array() ;
+        $i = 0;
+        error_log("collating 1") ;
+        foreach ($repositorySettings["settings"] as $key => $values) {
+            error_log("collating 2") ;
+            if (in_array($key, $names) && $values["enabled"] =="on") {
+                $cname = '\Model\\'.$key ;
+                $moduleFactory = new $cname();
+                $moduleRepositoryFeature = $moduleFactory->getModel($this->params, "RepositoryFeature");
+                // @ todo maybe an interface check? is object something?
+//                $values=array("default_fieldset" =>array(0 => array($values))) ; }
+                if (!isset($values["hash"])) { $values["hash"] = "12345" ; }
+                $moduleRepositoryFeature->setValues($values) ;
+                $moduleRepositoryFeature->setRepository($repository) ;
+                $collated = $moduleRepositoryFeature->collate();
+                error_log("collating 3") ;
+                if (array_key_exists(0, $collated)==true) {
+                    foreach ($collated as $one_collated) {
+                        $enabledFeatures[$i]["module"] = $key  ;
+                        $enabledFeatures[$i]["values"] = $values  ;
+                        $enabledFeatures[$i]["model"] = $one_collated ;
+                        $i++; } }
+                else {
+                    $enabledFeatures[$i]["module"] = $key  ;
+                    $enabledFeatures[$i]["values"] = $values  ;
+                    $enabledFeatures[$i]["model"] = $collated  ;
+                    $i++; } } }
+
+        return array("features" => $enabledFeatures) ;
     }
 
 }
