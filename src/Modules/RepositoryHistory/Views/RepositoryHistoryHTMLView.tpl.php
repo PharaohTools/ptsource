e29c2492a9bdf55c61623dbf72a7c82e4fe2cb8b
David Amanshia <phpengine@pharaohtools.com>
2016-07-31 16:18:30 +0100

    more relevant menu items in repo history and fixed broken links

--- src/Modules/RepositoryHistory/Views/RepositoryHistoryHTMLView.tpl.php
+++ src/Modules/RepositoryHistory/Views/RepositoryHistoryHTMLView.tpl.php
@@ -30,32 +30,29 @@
 					</ul>
 					<!-- /.nav-second-level -->
 				</li>
-				<li>
-					<a href="/index.php?control=BuildConfigure&action=new"class=" hvr-bounce-in"><i class="fa fa-edit fa-fw hvr-bounce-in"></i> New Repository</a>
-				</li>
-				<li>
-					<a href="/index.php?control=BuildConfigure&action=copy"class=" hvr-bounce-in"><i class="fa fa-edit fa-fw hvr-bounce-in"></i> Copy Repository</a>
-				</li>
-				<li>
-					<a href="/index.php?control=RepositoryHistory&action=show " class="active  hvr-bounce-in"><i class="fa fa-bars fa-fw hvr-bounce-in"></i> All Repositories</a>
-				</li>
-				<!--<li>
-					<a href="/index.php?control=Monitors&action=DefaultHistory" class=" hvr-bounce-in"><i class="fa fa-history fa-fw hvr-bounce-in"></i> History<span class="fa arrow"></span></a>
-				</li> -->
+                <li>
+                    <a href="/index.php?control=FileBrowser&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"] ; ?>" class="hvr-bounce-in">
+                        <i class="fa fa-folder-open-o hvr-bounce-in"></i> File Browser
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?control=BuildMonitor&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"] ; ?>" class="hvr-bounce-in">
+                        <i class="fa fa-bar-chart-o hvr-bounce-in"></i> Monitors
+                    </a>
+                </li>
+                <li>
+                    <a href="/index.php?control=RepositoryHistory&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"] ; ?>" class="hvr-bounce-in">
+                        <i class="fa fa-history fa-fw hvr-bounce-in"></i> History <span class="badge"><?php echo $pageVars["data"]["history_count"] ; ?></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/index.php?control=RepositoryHome&action=delete&item=<?php echo $pageVars["data"]["repository"]["project-slug"] ; ?>" class="hvr-bounce-in">
+                        <i class="fa fa-trash fa-fw hvr-bounce-in"></i> Delete
+                    </a>
+                </li>
 			</ul>
 		</div>
 		<br />
-
-<!--		<div class="alert alert-info">-->
-<!--			<h4>Running Builds </h4>-->
-<!--			<div id="runningBuilds">-->
-<!--				<p>-->
-<!--					No repositorys currently being executed-->
-<!--				</p>-->
-<!--				-->
-<!--			</div>-->
-<!--		</div>-->
-
 	</div>
 
 	<div class="col-lg-9">
@@ -63,30 +60,37 @@
 <!--			<h2 class="text-uppercase text-light"><a href="/"> PTSource - Pharaoh Tools</a></h2>-->
 
 			<div class="row clearfix no-margin">
-				<h4 class="text-uppercase text-light">All Repositories</h4>
+				<h4 class="text-uppercase text-light">Commit History</h4>
 				<!--
 				<h3>
 				<a class="lg-anchor text-light" href="/index.php?control=RepositoryHistory&action=show">
 				Build History <i style="font-size: 18px;" class="fa fa-chevron-right"></i></a>
 				</h3>
 				-->
+
+                <?php
+
+                echo $this->renderWidgetPosition('IdentifierSelector', 'default') ;
+
+                ?>
+
 				<div role="tabpanel grid">
 
                     <!-- Nav tabs -->
-                    <ul class="nav nav-tabs" role="tabhistory">
-                        <li role="presentation" class="active">
-                            <a onclick="showFilteredRows('all'); return false ;">All</a>
-                        </li>
-                        <li role="presentation">
-                            <a onclick="showFilteredRows('success'); return false ;">All Success</a>
-                        </li>
-                        <li role="presentation">
-                            <a onclick="showFilteredRows('failure'); return false ;">All Failed</a>
-                        </li>
-                        <li role="presentation">
-                            <a onclick="showFilteredRows('unstable'); return false ;">All Unstable</a>
-                        </li>
-                    </ul>
+<!--                    <ul class="nav nav-tabs" role="tabhistory">-->
+<!--                        <li role="presentation" class="active">-->
+<!--                            <a onclick="showFilteredRows('all'); return false ;">All</a>-->
+<!--                        </li>-->
+<!--                        <li role="presentation">-->
+<!--                            <a onclick="showFilteredRows('success'); return false ;">All Success</a>-->
+<!--                        </li>-->
+<!--                        <li role="presentation">-->
+<!--                            <a onclick="showFilteredRows('failure'); return false ;">All Failed</a>-->
+<!--                        </li>-->
+<!--                        <li role="presentation">-->
+<!--                            <a onclick="showFilteredRows('unstable'); return false ;">All Unstable</a>-->
+<!--                        </li>-->
+<!--                    </ul>-->
 
 					<!-- Tab panes -->
 					<div class="tab-content">
@@ -95,145 +99,34 @@
 							<div class="table table-striped table-bordered table-condensed">
                                 <div>
                                     <div class="blCell cellRowIndex">#</div>
-                                    <div class="blCell cellRowName">Repository</div>
-                                    <div class="blCell cellRowRun">Run Now</div>
-                                    <div class="blCell cellRowStatus">Status</div>
-                                    <div class="blCell cellRowSuccess">Success</div>
-                                    <div class="blCell cellRowFailure">Failure</div>
-                                    <div class="blCell cellRowDuration">Duration</div>
+                                    <div class="blCell cellRowMessage">Message</div>
+                                    <div class="blCell cellRowAuthor">Author</div>
+                                    <div class="blCell cellRowDate">Date</div>
+                                    <div class="blCell cellRowHash">Hash</div>
+<!--                                    <div class="blCell cellRowFailure">Failure</div>-->
+<!--                                    <div class="blCell cellRowDuration">Duration</div>-->
                                 </div>
 							<div class="allBuildRows table-hover">
 
 							<?php
 
 							$i = 1;
-							foreach ($pageVars["data"]["commits"] as $repositorySlug => $repositoryDetails) {
 
-
-                                if ($repositoryDetails["last_status"] === true) {
-                                    $successFailureClass = "successRow"  ; }
-                                else if ($repositoryDetails["last_status"] === false) {
-                                    $successFailureClass = "failureRow" ; }
-                                else {
-                                    $successFailureClass = "unstableRow" ; }
+                            foreach ($pageVars["data"]["commits"] as $commitDetails) {
 
                                 ?>
 
-							<div class="repositoryRow <?php echo $successFailureClass ?>" id="blRow_<?php echo $repositorySlug; ?>" >
+							<div class="commitRow" id="blRow_<?php echo $repositorySlug; ?>" >
 							<div class="blCell cellRowIndex" scope="row"><?php echo $i; ?> </div>
-							<div class="blCell cellRowName"><a href="/index.php?control=RepositoryHome&action=show&item=<?php echo $repositorySlug; ?>" class="pipeName"><?php echo $repositoryDetails["project-name"]; ?>  </a> </div>
-							
-							<div class="blCell cellRowRun">
-							<?php
-							echo '<a href="/index.php?control=PipeRunner&action=start&item=' . $repositoryDetails["project-slug"] . '">';
-							echo '<i class="fa fa-play fa-2x hvr-grow-shadow" style="color:rgb(13, 193, 42);"></i></a>';
-							?>
-							</div>
-							<div  class="blCell cellRowStatus"
+                            <div class="blCell cellRowMessage"><a href="/index.php?control=CommitDetails&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"]; ?>&identifier=<?php echo $pageVars["data"]["identifier"] ; ?>" class="pipeName"><?php echo $commitDetails["message"]; ?>  </a> </div>
+                            <div class="blCell cellRowAuthor"><a href="/index.php?control=CommitDetails&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"]; ?>&identifier=<?php echo $pageVars["data"]["identifier"] ; ?>" class="pipeName"><?php echo $commitDetails["author"]; ?>  </a> </div>
+                            <div class="blCell cellRowDate"><a href="/index.php?control=CommitDetails&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"]; ?>&identifier=<?php echo $pageVars["data"]["identifier"] ; ?>" class="pipeName"><?php echo $commitDetails["date"]; ?>  </a> </div>
+                            <div class="blCell cellRowHash"><a href="/index.php?control=CommitDetails&action=show&item=<?php echo $pageVars["data"]["repository"]["project-slug"]; ?>&identifier=<?php echo $pageVars["data"]["identifier"] ; ?>" class="pipeName"><?php echo substr($commitDetails["commit"], 0, 6); ?>  </a> </div>
                                 <?php
 
-                                if ($repositoryDetails["last_status"] === true) {
-                                    echo ' style="background-color:rgb(13, 193, 42);" '; }
-                                else if ($repositoryDetails["last_status"] === false) {
-                                    echo ' style="background-color:#D32B2B" '; }
-                                else {
-                                    echo ' style="background-color:gray" '; }
+                                $i++ ;
+                                }
                                 ?>
-
-                                >
-
-                                <?php
-
-                                echo '<p> #'.$repositoryDetails["last_run_repository"].'</p>' ;
-
-							?>
-
-							</div>
-							
-							<div class="blCell cellRowSuccess">
-							<?php
-
-                            $today = new DateTime(); // This object represents current date/time
-                            $actualToday = $today; // copy original for display
-                            $today->setTime( 0, 0, 0 ); // reset time part, to prevent partial comparison
-
-                            if ($repositoryDetails["last_success"] != false) {
-
-                                $match_date = new DateTime(date('d.m.Y H:i', $repositoryDetails["last_success"]));
-
-//                                var_dump($match_date) ;
-
-                                $diff = $today->diff( $match_date );
-                                $diffDays = (integer)$diff->format( "%R%a" ); // Extract days count in interval
-
-								$date = date($repositoryDetails["last_success"]);
-                                if( $diffDays == 0 ) {
-                                    echo date_format($match_date, 'g:ia')." Today"; }
-                                else if( $diffDays == -1 ) {
-                                    echo date_format($match_date, 'g:ia')." Yesterday"; }
-                                else {
-                                    echo date_format($match_date, 'g:ia \o\n D jS M Y'); }
-								echo ' #(' . $repositoryDetails["last_success_repository"] . ')'; }
-                            else {
-								echo 'N/A'; }
-							?>
-							</div>
-							<div class="blCell cellRowFailure">
-							<?php
-							if ($repositoryDetails["last_fail"] != false) {
-
-                                $match_date = new DateTime(date('d.m.Y H:i', $repositoryDetails["last_fail"]));
-//                                var_dump($match_date) ;
-
-                                $diff = $today->diff( $match_date );
-                                $diffDays = (integer)$diff->format( "%R%a" ); // Extract days count in interval
-
-                                $date = date($repositoryDetails["last_fail"]);
-                                if( $diffDays == 0 ) {
-                                    echo date_format($match_date, 'g:ia')." Today"; }
-                                else if( $diffDays == -1 ) {
-                                    echo date_format($match_date, 'g:ia')." Yesterday"; }
-                                else {
-                                    echo date_format($match_date, 'g:ia \o\n D jS M Y'); }
-                                echo ' #(' . $repositoryDetails["last_fail_repository"] . ')';}
-                            else {
-                                echo 'N/A'; }
-
-							?>
-							</div>
-							<div class="blCell cellRowDuration">
-							<?php
-							if ($repositoryDetails["duration"] != false) {
-								echo $repositoryDetails["duration"] . ' seconds';
-							} else {
-								echo 'N/A';
-							}
-							?>
-							</div>
-							<!--
-							<div>
-							<?php
-							if ($repositoryDetails["has_parents"] === true) {
-							    echo '<img class="historyImage" src="/Assets/Modules/RepositoryHistory/image/tick.png" />' ; }
-							else {
-							    echo '<img class="historyImage" src="/Assets/Modules/RepositoryHistory/image/cross.png" />' ; }
-							?>
-							</div>
-							<div>
-							<?php
-							if ($repositoryDetails["has_children"] === true) {
-							    echo '<img class="historyImage" src="/Assets/Modules/RepositoryHistory/image/tick.png" />' ; }
-							else {
-							    echo '<img class="historyImage" src="/Assets/Modules/RepositoryHistory/image/cross.png" />' ; }
-							?>
-							</div>
-							-->
-							</div>
-							<?php
-							$i++;
-							}
-							?>
-
 							</div>
 							</div>
 						</div>
@@ -249,3 +142,5 @@
 </div><!-- /.container -->
 <link rel="stylesheet" type="text/css" href="/Assets/Modules/RepositoryHistory/css/repositoryhistory.css">
 <script type="text/javascript" src="/Assets/Modules/RepositoryHistory/js/repositoryhistory.js"></script>
+
+
