1064391e4bd502ba718d3e0f7582d75a0342d78b
David Amanshia <phpengine@pharaohtools.com>
2016-11-15 22:40:23 +0000

    correct app name in bundle

--- src/Modules/AssetPublisher/Libraries/npm/app.js
+++ src/Modules/AssetPublisher/Libraries/npm/app.js
@@ -15,7 +15,7 @@ var $ = require('jquery'),
 
 var file_require_string = "" ;
 mainFiles.forEach(function (filePath) {
-    filePath = filePath.replace("/opt/pttrack/pttrack/src/Modules/", "../../");
+    filePath = filePath.replace("/opt/ptsource/ptsource/src/Modules/", "../../");
     file_require_string += 'require("'+filePath+'") ; ';
 });
 
