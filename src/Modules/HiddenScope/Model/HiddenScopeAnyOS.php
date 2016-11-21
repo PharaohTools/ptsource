f1a4c5ae0a693e9aad6ae6d12c862b353e042d33
David Amanshia <phpengine@pharaohtools.com>
2016-11-21 04:49:24 +0000

    hidden scope

--- src/Modules/HiddenScope/Model/HiddenScopeAnyOS.php
+++ src/Modules/HiddenScope/Model/HiddenScopeAnyOS.php
@@ -2,7 +2,7 @@
 
 Namespace Model;
 
-class HiddenScopeLinuxUnix extends Base {
+class HiddenScopeAnyOS extends Base {
 
     // Compatibility
     public $os = array("any") ;
@@ -24,78 +24,17 @@ class HiddenScopeLinuxUnix extends Base {
             array(
                 "type" => "boolean",
                 "optional" => true,
-                "name" => "Enable Public Scope for Repositories?"
+                "name" => "Hide this Repo?"
             ),
-            "public_pages" =>
-            array(
-                "type" => "boolean",
-                "optional" => true,
-                "name" => "Make Repository Pages Public?"
-            ),
-            "public_read" =>
-            array(
-                "type" => "boolean",
-                "optional" => true,
-                "name" => "Allow Public Code Reads?"
-            ),
-            "public_write" =>
-            array(
-                "type" => "boolean",
-                "optional" => true,
-                "name" => "Allow Public Code Writes?"
-            ),
+//            "hidden_to_all" =>
+//            array(
+//                "type" => "boolean",
+//                "optional" => true,
+//                "name" => "Hide from members also?"
+//            ),
         );
         return $ff ;
     }
 
-    public function getEventNames() {
-        return array_keys($this->getEvents());
-    }
-
-    // @todo need thee cron execution event to do this
-    public function getEvents() {
-        $ff = array(
-            "getPublicLinks" => array(
-                "getPublicRepositories",
-            ),
-        );
-        return $ff ;
-    }
-
-    public function getPublicRepositories() {
-        $this->params["echo-log"] = true ;
-        $this->params["php-log"] = true ;
-        $repositories = $this->getRepositories() ;
-        $public_repositories = array() ;
-//        var_dump("<pre>","reps:", $repositories, "</pre>") ;
-//        die() ;
-        foreach ($repositories as $repository_slug => $repository) {
-            if ($repository["settings"]["HiddenScope"]["enabled"] == "on") {
-                if ($repository["settings"]["HiddenScope"]["public_pages"] == "on") {
-                    $public_repositories[] = $repository ; } } }
-        $public_repositories_html = $this->getHTMLFromRepositories($public_repositories) ;
-        \Model\RegistryStore::setValue('public_links', $public_repositories_html) ;
-        return $public_repositories ;
-    }
-
-    public function getRepositories() {
-        $repositoryFactory = new \Model\Repository() ;
-        $repository = $repositoryFactory->getModel($this->params);
-        $repos = $repository->getRepositories();
-        return $repos ;
-    }
-
-    public function getHTMLFromRepositories($public_repositories) {
-        $html = "" ;
-        if (count($public_repositories)>0) {
-            $html .= "<h3><strong>Public Repositories:</strong></h3>" ;
-            foreach ($public_repositories as $public_repository) {
-                $html .= "<div>" ;
-                $html .= "    <a target='_blank' href='index.php?control=RepositoryHome&action=show&item={$public_repository["project-slug"]}' > " ;
-                $html .= "        {$public_repository["project-name"]} " ;
-                $html .= "    </a>" ;
-                $html .= "</div>" ; } }
-        return $html ;
-    }
 
 }
--- src/Modules/HiddenScope/info.HiddenScope.php
+++ src/Modules/HiddenScope/info.HiddenScope.php
@@ -17,29 +17,16 @@ class HiddenScopeInfo extends PTConfigureBase {
     }
 
     public function routeAliases() {
-        return array("publicscope"=>"HiddenScope");
-    }
-
-    public function events() {
-        return array("getPublicLinks");
+        return array("hiddenscope"=>"HiddenScope");
     }
 
     public function repositorySettings() {
-        return array("enabled", "public_pages", "public_read", "public_write");
-    }
-
-    public function configuration() {
-        return array(
-            "enable_public"=> array(
-                "type" => "boolean",
-                "default" => "",
-                "label" => "Enable use of Public Access for Pages and Repositories?", ),
-        );
+        return array("enabled");
     }
 
     public function helpDefinition() {
        $help = <<<"HELPDATA"
-    This extension provides integration with HiddenScope as a Build Step. It provides code
+    This extension provides integration with HiddenScope as a Repository Setting. It provides code
     functionality, but no extra CLI commands.
 
     HiddenScope
