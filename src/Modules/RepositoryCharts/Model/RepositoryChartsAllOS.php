f8909d5ac8205c922b8a3ebffec22de077eb05a0
David Amanshia <phpengine@pharaohtools.com>
2016-11-24 00:30:09 +0000

    repo charts integrating with pretty stats

--- src/Modules/RepositoryCharts/Model/RepositoryChartsAllOS.php
+++ src/Modules/RepositoryCharts/Model/RepositoryChartsAllOS.php
@@ -14,15 +14,17 @@ class RepositoryChartsAllOS extends Base {
     // Model Group
     public $modelGroup = array("Default") ;
 
+    public function __construct($params) {
+        parent::__construct($params) ;
+        $this->getLibraries() ;
+    }
+
     public function getData() {
         $ret["repository"] = $this->getRepository();
-        $ret["features"] = $this->getRepositoryFeatures();
+        $ret["repository_charts"] = $this->getRepositoryCharts($ret["repository"]);
         $ret["history"] = $this->getCommitHistory();
-        $ret["is_https"] = $this->isSecure();
         $ret["user"] = $this->getLoggedInUser();
         $ret["current_user_role"] = $this->getCurrentUserRole($ret["user"]);
-        $ret["readme"] = $this->getReadmeData($ret["repository"]) ;
-        $ret = array_merge($ret, $this->getIdentifier()) ;
         return $ret ;
     }
 
@@ -32,7 +34,6 @@ class RepositoryChartsAllOS extends Base {
         $this->params["user"] = $signup->getLoggedInUserData() ;
         return $this->params["user"] ;
     }
-
 
     public function getCurrentUserRole($user = null) {
         if ($user == null) { $user = $this->getLoggedInUser(); }
@@ -45,6 +46,17 @@ class RepositoryChartsAllOS extends Base {
         return $ret ;
     }
 
+    protected function getLibraries() {
+        $libDir = dirname(__DIR__).DS."Libraries".DS ;
+        require_once $libDir."gitter".DS."vendor".DS."autoload.php" ;
+        foreach (glob("{$libDir}GitPrettyStats".DS."Charts".DS."*.php") as $filename) {
+            require_once $filename; }
+        foreach (glob("{$libDir}GitPrettyStats".DS."Providers".DS."*.php") as $filename) {
+            require_once $filename; }
+        foreach (glob("{$libDir}GitPrettyStats".DS."*.php") as $filename) {
+            require_once $filename; }
+    }
+
     protected function getRepository() {
         $repositoryFactory = new \Model\Repository() ;
         $repository = $repositoryFactory->getModel($this->params);
@@ -52,49 +64,33 @@ class RepositoryChartsAllOS extends Base {
         return $r ;
     }
 
-    protected function getReadmeData($repository) {
-        $identifier = 'HEAD' ;
-        $cur_dir = getcwd() ;
-        chdir(REPODIR.DS.$repository["project-slug"]) ;
-        $command = 'git ls-tree --full-tree '.$identifier ;
-        exec($command, $output) ;
-        $files = $this->parseLSTree($output) ;
-        if (in_array("README.md", $files)) { $load_readme_res = true ; }
-        else { $load_readme_res = false ; }
-        if ($load_readme_res == false) {
-            $rd_res["exists"] = false ; }
-        else {
-            $rd_res["exists"] = true ;
-            $command = 'git show '.$identifier.':README.md ' ;
-            $rd_res["raw"] = shell_exec($command) ;
-            require_once(dirname(__DIR__).DS."Libraries".DS."parsedown".DS."Parsedown.php") ;
-            $Parsedown = new \Parsedown();
-            $rd_res["md"] = $Parsedown->text($rd_res["raw"]); }
-        chdir($cur_dir);
-        return $rd_res ;
-    }
+    protected function getRepositoryCharts($repo) {
 
-    protected function parseLSTree($output) {
-        $files = array() ;
-        foreach ($output as $line) {
-            $parts = explode(' ', $line) ;
-            $further_parts = explode("\t", $parts[2]) ;
-            $files[] = $further_parts[1] ; }
-        return $files ;
-    }
+        $client = new \Gitter\Client;
 
-    public function getIdentifier() {
-        // @todo get default branch if there is one
-        if (!isset($this->params["identifier"])) { $this->params["identifier"] = "master" ; }
-        $identifier = array("identifier" => $this->params["identifier"]);
-        return $identifier ;
-    }
+        $loc = REPODIR.DS.$this->params["item"] ;
+        $repository = $client->getRepository($loc);
+//        $gpc = new \GitPrettyStats\Repository($loc, $client) ;
 
-    protected function getRepositoryFeatures() {
-        $repositoryFactory = new \Model\Repository() ;
-        $repository = $repositoryFactory->getModel($this->params);
-        $r = $repository->getRepositoryFeatures($this->params["item"]);
-        return $r ;
+//        var_dump($gpc) ;
+
+        $repository->addStatistics(array(
+            new \Gitter\Statistics\Contributors,
+            new \Gitter\Statistics\Date,
+            new \Gitter\Statistics\Day,
+            new \Gitter\Statistics\Hour
+        ));
+//        print_r($gpc->getStatistics());
+//        $stats = $repository->getStatistics() ;
+        $stats2 = $repository->statistics() ;
+//
+//        ob_start();
+//        var_dump( $stats2) ;
+//        $res = ob_get_clean() ;
+//
+//        file_put_contents("/tmp/log", $res) ;
+        $fc = $repository->getFirstCommitDate();
+        return $stats2 ;
     }
 
     protected function getCommitHistory() {
@@ -103,18 +99,6 @@ class RepositoryChartsAllOS extends Base {
         $commitParams["amount"] = 10 ;
         $repository = $repositoryFactory->getModel($commitParams, "RepositoryCommits");
         return $repository->getCommits();
-    }
-
-    protected function isSecure() {
-        return
-            (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-            || $_SERVER['SERVER_PORT'] == 443;
-    }
-
-    public function deleteRepository() {
-        $repositoryFactory = new \Model\Repository() ;
-        $repository = $repositoryFactory->getModel($this->params);
-        return $repository->deleteRepository($this->params["item"]);
     }
 
     public function userIsAllowedAccess() {
