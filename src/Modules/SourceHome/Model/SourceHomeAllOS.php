d3cb07042d2f082f7b4a1207ac3751c04be834d6
David Amanshia <phpengine@pharaohtools.com>
2016-11-29 14:25:49 +0000

    started output of numbers for login page

--- src/Modules/SourceHome/Model/SourceHomeAllOS.php
+++ src/Modules/SourceHome/Model/SourceHomeAllOS.php
@@ -16,18 +16,14 @@ class SourceHomeAllOS extends Base {
 
 
     public function getData() {
-//        $ret["issues"] = $this->getIssues();
-//        $ret["all-issue-counts"] = $this->getAllIssueCounts();
-        $ret["all-issue-counts"] = 10 ;
-//        $ret["my-issue-watching-count"] = $this->getMyIssueCount("watching");
-//        $ret["my-issue-submitted-count"] = $this->getMyIssueCount("submitted");
-//        $ret["my-issue-assigned-count"] = $this->getMyIssueCount("assigned");
-        // jobs
-//        $ret["job_count"] = $this->getJobCount();  // adds 0.5 secs, can stay
-//        $ret["my_job_created_count"] = $this->getJobCreatedCount();
-//        $ret["my_job_member_count"] = $this->getJobMemberCount();
-        // user
+        $ret["all_repositories"] = $this->getRepositories();
         $ret["user"] = $this->getLoggedInUser();
+        if ($ret["user"] !== false) {
+            $ret["my_owned_repositories"] = $this->getMyRepositoriesCount($ret["all_repositories"], $ret["user"]);
+            $ret["my_member_repositories"] = $this->getMemberRepositoriesCount($ret["all_repositories"], $ret["user"]);
+            $ret["all_teams"] = $this->getRepositories();
+            $ret["my_owned_teams"] = $this->getMyTeamsCount($ret["all_teams"], $ret["user"]);
+            $ret["my_member_teams"] = $this->getMemberTeamsCount($ret["all_teams"], $ret["user"]); }
         return $ret ;
     }
 
@@ -36,6 +32,47 @@ class SourceHomeAllOS extends Base {
         $signup = $signupFactory->getModel($this->params);
         $this->params["user"] = $signup->getLoggedInUserData() ;
         return $this->params["user"] ;
+    }
+
+    public function getRepositories() {
+        $repositoryFactory = new \Model\Repository() ;
+        $repository = $repositoryFactory->getModel($this->params);
+        $repos = $repository->getRepositories();
+        return $repos ;
+    }
+
+    public function getMyRepositoriesCount($all_repos, $user) {
+        $my_repos = 0 ;
+        foreach ($all_repos as $one_repo) {
+            if ($one_repo["project-owner"] == $user->username) {
+                $my_repos ++ ; } }
+        return $my_repos ;
+    }
+
+    public function getMemberRepositoriesCount($all_repos, $user) {
+        $member_repos = 0 ;
+        foreach ($all_repos as $one_repo) {
+            $repo_members = explode(",", $one_repo["project-members"]) ;
+            if ( in_array($user->username, $repo_members)) {
+                $member_repos ++ ; } }
+        return $member_repos ;
+    }
+
+    public function getMyTeamsCount($all_teams, $user) {
+        $my_teams = 0 ;
+        foreach ($all_teams as $one_team) {
+            if ($one_team["project-owner"] == $user->username) {
+                $my_teams ++ ; } }
+        return $my_teams ;
+    }
+
+    public function getMemberTeamsCount($all_teams, $user) {
+        $member_teams = 0 ;
+        foreach ($all_teams as $one_team) {
+            $team_members = explode(",", $one_team["project-members"]) ;
+            if ( in_array($user->username, $team_members)) {
+                $member_teams ++ ; } }
+        return $member_teams ;
     }
 
     public function findModuleNames($params) {
