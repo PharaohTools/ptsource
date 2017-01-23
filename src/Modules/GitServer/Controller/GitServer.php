3861c7f4fbf7112793bcfc8616cb1197aad7ed15
David Amanshia <phpengine@pharaotools.com>
2017-01-23 16:42:48 +0000

    git server controller formatting

--- src/Modules/GitServer/Controller/GitServer.php
+++ src/Modules/GitServer/Controller/GitServer.php
@@ -8,12 +8,12 @@ class GitServer extends Base {
       $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
       // if we don't have an object, its an array of errors
       $this->content = $pageVars ;
-      if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-      if(in_array($pageVars["route"]["action"], array("serve") ) ) {
-            $this->content["data"] = $thisModel->backendData(); }
-
-        $this->content["route"]["extraParams"] ;
-        $this->content["output-format"] = "git" ;
+      if (is_array($thisModel)) {
+          return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+      if ($pageVars["route"]["action"] === 'serve') {
+          $this->content["data"] = $thisModel->backendData(); }
+          $this->content["route"]["extraParams"] ;
+          $this->content["output-format"] = "git" ;
       return array ("type"=>"view", "view"=>"gitServer", "pageVars"=>$this->content);
     }
 
