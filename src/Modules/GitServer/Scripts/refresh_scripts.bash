cbb99b068412ef8955e3405d8f26e438f0c3fad2
David Amanshia <phpengine@pharaotools.com>
2017-02-02 10:43:52 +0000

    fixed paths in dev refresh scfipte

--- src/Modules/GitServer/Scripts/refresh_scripts.bash
+++ src/Modules/GitServer/Scripts/refresh_scripts.bash
@@ -2,17 +2,17 @@
 
 userdel ptgit
 rm -rf /home/ptgit
-useradd -s /bin/sh -m -d /home/ptgit ptgit
+useradd -s /bin/bash -m -d /home/ptgit ptgit
 usermod -p `date +%s | sha256sum | base64 | head -c 32 ; echo` ptgit
 mkdir -p /Scripts/
 mkdir -p /home/ptgit/ptsource/
-cp /opt/ptsource/ptsource/src/Modules/GitServer/Scripts/* /Scripts/
+cp /opt/ptsource/ptsource/src/Modules/GitServer/Scripts/* /PTSourceScripts/
 cp /opt/ptsource/ptsource/src/Modules/GitServer/Scripts/* /home/ptgit/ptsource/
-chown -R root /Scripts/
-chmod -R 700 /Scripts/
-chmod -R +x /Scripts/
+chown -R root /PTSourceScripts/
+chmod -R 755 /PTSourceScripts/
+chmod -R +x /PTSourceScripts/
 chown -R ptgit /home/ptgit/ptsource/
-chmod -R 700 /home/ptgit/ptsource/
+chmod -R 755 /home/ptgit/ptsource/
 chmod -R +x /home/ptgit/ptsource/
 service ssh restart
 chown -R ptsource:ptgit /opt/ptsource/repositories/*
