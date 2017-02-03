ff894d4e592f8761c5b3f83609c2a695246cea91
David Amanshia <phpengine@pharaotools.com>
2017-02-03 21:55:11 +0000

    restored the http auth script i broke

---
+++ src/Modules/Signup/Scripts/auth_check.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+// Read from stdin. First line is the username, second line is the password.
+$handle = fopen ("php://stdin","r");
+$username = trim(fgets($handle));
+$password = trim(fgets($handle));
+
+require_once(dirname(dirname(dirname(__DIR__))).DIRECTORY_SEPARATOR."Constants.php");
+require_once(dirname(dirname(dirname(__DIR__))).DIRECTORY_SEPARATOR."AutoLoad.php");
+
+$autoLoader = new \Core\autoLoader();
+$autoLoader->launch();
+
+if (userValid($username, $password)==false) {
+    exit (1) ; }
+
+//echo "username/password allowed for user $username\n";
+exit (0);
+
+function userValid($username, $password) {
+    // @todo should check if public scope is enabled first probably before authing anon user
+    if ($username=="anon") { return true ; }
+    $signupFactory = new \Model\Signup() ;
+    $signup = $signupFactory->getModel(array(), "Default") ;
+    $res = $signup->checkLogin($username, $password, false) ;
+    if ($res["status"] == true) { return true ; }
+    return false ;
+}
+
+?>
