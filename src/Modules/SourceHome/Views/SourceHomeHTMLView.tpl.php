b3c5a7a46ccd1bd9c0359f173c6b6f4577fe4ae9
David Amanshia <phpengine@pharaotools.com>
2017-02-06 18:57:45 +0000

    source home using type casting on purpose

--- src/Modules/SourceHome/Views/SourceHomeHTMLView.tpl.php
+++ src/Modules/SourceHome/Views/SourceHomeHTMLView.tpl.php
@@ -43,7 +43,7 @@
                 </li>
 
                 <?php
-                    if ($pageVars['data']['user'] !== false && ($pageVars['data']['user']->role === 1)) {
+                    if ($pageVars['data']['user'] !== false && ($pageVars['data']['user']->role == "1")) {
                 ?>
 
                     <li>
