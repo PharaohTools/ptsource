7525c03800238552306f6c4d46df986685574f50
David Amanshia <phpengine@pharaotools.com>
2017-02-21 23:46:15 +0000

    oauth key valid first draft

--- src/Modules/Signup/Scripts/auth_check.php
+++ src/Modules/Signup/Scripts/auth_check.php
@@ -11,11 +11,15 @@ require_once(dirname(dirname(dirname(__DIR__))).DIRECTORY_SEPARATOR."AutoLoad.ph
 $autoLoader = new \Core\autoLoader();
 $autoLoader->launch();
 
-if (userValid($username, $password)==false) {
-    exit (1) ; }
+if (userValid($username, $password)=== true) {
+    exit (0) ; }
 
+if (oauthKeyValid($username, $password)=== true) {
+    exit (0) ; }
+
+// No methods left to try
 //echo "username/password allowed for user $username\n";
-exit (0);
+exit (1);
 
 function userValid($username, $password) {
     // @todo should check if public scope is enabled first probably before authing anon user
@@ -23,7 +27,21 @@ function userValid($username, $password) {
     $signupFactory = new \Model\Signup() ;
     $signup = $signupFactory->getModel(array(), "Default") ;
     $res = $signup->checkLogin($username, $password, false) ;
-    if ($res["status"] == true) { return true ; }
+    if ($res["status"] === true) {
+        // dont just return true here
+        return true ; }
+    return false ;
+}
+
+function oauthKeyValid($username, $password) {
+    // @todo should check if public scope is enabled first probably before authing anon user
+    if ($username=="anon") { return true ; }
+    $uoakFactory = new \Model\UserOAuthKey() ;
+    $uoak = $uoakFactory->getModel(array(), "Default") ;
+    $res = $uoak->checkOAuth($username, $password, false) ;
+    if ($res["status"] === true) {
+        // don't just return true here
+        return true ; }
     return false ;
 }
 
