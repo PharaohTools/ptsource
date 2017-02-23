b3a775bd7f6908e003089d51cfa5f3d6569216cb
David Amanshia <phpengine@pharaotools.com>
2017-02-23 09:59:27 +0000

    fixed routes for integrations module

--- src/Modules/Integrations/info.Integrations.php
+++ src/Modules/Integrations/info.Integrations.php
@@ -6,7 +6,7 @@ class IntegrationsInfo extends PTConfigureBase {
 
     public $hidden = false;
 
-    public $name = "Manage the modules used in PTConfigure";
+    public $name = "View the Integrations in Pharaoh Source";
 
     public function __construct() {
       parent::__construct();
@@ -19,35 +19,35 @@ class IntegrationsInfo extends PTConfigureBase {
     }
 
     public function routeAliases() {
-      return array("module-manager"=>"Integrations", "modulemanager"=>"Integrations");
+      return array("integrations"=>"Integrations");
     }
 
     public function helpDefinition() {
       $help = '
   The Module Manager allows you to manage modules. Install, Ensure, Uninstall, Enable, Disable.
 
-  Integrations, module-manager, modulemanager
+  Integrations, integrations
 
         - install
         Installs the latest version of a module. If a module of the same name already exists in your Extensions directory,
         an error will be thrown.
-        example: '.PHARAOH_APP.' module-manager install --module-name="MyModule" --module-source="http://git.cleo-modules.com/MyModule.git"
+        example: '.PHARAOH_APP.' integrations install --module-name="MyModule" --module-source="http://git.cleo-modules.com/MyModule.git"
 
         - ensure
         Ensures the existence of a module. The module will only be installed if it currently doesn\'t exist.
-        example: '.PHARAOH_APP.' module-manager ensure --module-name="MyModule" --module-source="http://git.cleo-modules.com/MyModule.git"
+        example: '.PHARAOH_APP.' integrations ensure --module-name="MyModule" --module-source="http://git.cleo-modules.com/MyModule.git"
 
         - uninstall
         Uninstalls a Module. This will delete all of the files for this Module
-        example: '.PHARAOH_APP.' module-manager enable --module-name="MyModule"
+        example: '.PHARAOH_APP.' integrations enable --module-name="MyModule"
 
         - enable
         Enables a Module. All installed Modules are enabled by default.
-        example: '.PHARAOH_APP.' module-manager enable --module-name="MyModule"
+        example: '.PHARAOH_APP.' integrations enable --module-name="MyModule"
 
         - disable
         Disables a Module. The files for this module will still exist, but none will be automatically loaded during execution.
-        example: '.PHARAOH_APP.' module-manager disable --module-name="MyModule"
+        example: '.PHARAOH_APP.' integrations disable --module-name="MyModule"
     ';
       return $help ;
     }
