b5c0c5e88b7be6331a75f3139786774782cdcfc0
David Amanshia <phpengine@pharaotools.com>
2017-02-27 15:59:47 +0000

    default integration

--- src/Modules/Integrations/Model/IntegrationsWebAnyOS.php
+++ src/Modules/Integrations/Model/IntegrationsWebAnyOS.php
@@ -15,8 +15,8 @@ class IntegrationsWebAnyOS extends BasePHPApp {
     public $modelGroup = array("Web") ;
 
     public function getData() {
-        $ret["installed_integrations"] = $this->findAllIntegrationNames();
-        $ret["available_integrations"] = $this->getAvailableIntegrations();
+        $ret["installed_integrations"] = $this->getEnabledIntegrations();
+        $ret["available_integrations"] = $this->findAllIntegrationNames();
         return $ret ;
     }
 
@@ -33,44 +33,56 @@ class IntegrationsWebAnyOS extends BasePHPApp {
         return $integrationNames;
     }
 
-    private function getAvailableIntegrations() {
+    private function getEnabledIntegrations() {
 
         $ray = array(
-            "TestIntegrationOne" => array(
-                "repo_url" => "http://www.github.com/PharaohIntegrations/test-one.git",
-                "description" => "This is the first test description",
-                "name" => "Test Integration One",
-                "group" => "test_integrations",
+            "PharaohBuild" => array(
+                "repo_url" => "http://www.github.com/PharaohTools/build.git",
+                "description" =>
+                    "This is the Pharaoh Build integration. This will allow you to integrate your repositories " .
+                    "with your build processes and results published by one or more Pharaoh Build Servers. " ,
+                "name" => "Pharaoh Build",
+                "image" => "/Assets/Modules/DefaultSkin/image/build-logo.png",
+                "manage_link" => "/index.php?control=ApplicationConfigure&action=show",
+                "group" => "pharaoh_tools",
                 "dependencies" => array("Logging", "SendEmail"),
             ),
-            "TestIntegrationTwo" => array(
-                "repo_url" => "http://www.github.com/PharaohIntegrations/test-two.git",
-                "description" => "This is the second test description",
-                "name" => "Test Integration Two",
-                "group" => "test_integrations",
+            "PharaohTrack" => array(
+                "repo_url" => "http://www.github.com/PharaohTools/track.git",
+                "description" =>
+                    "This is the Pharaoh Track integration. This will allow you to integrate your repositories " .
+                    "with your issues, jobs, milestones and more that are published by one or more Pharaoh Track Servers. " ,
+                "name" => "Pharaoh Track",
+                "image" => "/Assets/Modules/DefaultSkin/image/track-logo.png",
+                "manage_link" => "Pharaoh Track",
+                "group" => "pharaoh_tools",
                 "dependencies" => array("Logging", "SendEmail"),
             ),
-            "TestIntegrationThree" => array(
-                "repo_url" => "http://www.github.com/PharaohIntegrations/test-three.git",
-                "description" => "This is the third test description",
-                "name" => "Test Integration Three",
-                "group" => "test_integrations",
+            "PharaohManage" => array(
+                "repo_url" => "http://www.github.com/PharaohTools/manage.git",
+                "description" =>
+                    "This is the Pharaoh Build integration. This will allow you to integrate your repositories " .
+                    "with your build processes and results published by one or more Pharaoh Build Servers. " ,
+                "name" => "Pharaoh Manage",
+                "image" => "/Assets/Modules/DefaultSkin/image/manage-logo.png",
+                "manage_link" => "/index.php?control=ApplicationConfigure&action=show#gitserver",
+                "group" => "pharaoh_tools",
                 "dependencies" => array("Logging", "SendEmail"),
             ),
-            "TestIntegrationFour" => array(
-                "repo_url" => "http://www.github.com/PharaohIntegrations/test-four.git",
-                "description" => "This is the fourth test description",
-                "name" => "Test Integration Two",
-                "group" => "test_integrations",
-                "dependencies" => array("Logging", "SendEmail"),
-            ),
-            "DummyLinuxIntegration" => array(
-                "repo_url" => "http://www.github.com/PharaohIntegrations/test-two.git",
-                "description" => "This is the dummy linux integration test description",
-                "name" => "Dummy Linux Integration",
-                "group" => "dummy_integrations",
-                "dependencies" => array("Logging", "SendEmail"),
-            ),
+//            "TestIntegrationFour" => array(
+//                "repo_url" => "http://www.github.com/PharaohIntegrations/test-four.git",
+//                "description" => "This is the fourth test description",
+//                "name" => "Test Integration Two",
+//                "group" => "test_integrations",
+//                "dependencies" => array("Logging", "SendEmail"),
+//            ),
+//            "DummyLinuxIntegration" => array(
+//                "repo_url" => "http://www.github.com/PharaohIntegrations/test-two.git",
+//                "description" => "This is the dummy linux integration test description",
+//                "name" => "Dummy Linux Integration",
+//                "group" => "dummy_integrations",
+//                "dependencies" => array("Logging", "SendEmail"),
+//            ),
         );
         return $ray ;
     }
