73c733cf2ac268b94683aa639b00192b12003938
David Amanshia <phpengine@pharaotools.com>
2017-03-04 16:28:53 +0000

    changing user access from object to array

--- src/Modules/UserProfile/Model/UserProfileDeleteUserAllOS.php
+++ src/Modules/UserProfile/Model/UserProfileDeleteUserAllOS.php
@@ -50,38 +50,35 @@ class UserProfileDeleteUserAllOS extends Base {
     private function userAlreadyExists() {
         $allusers = $this->getAllUserDetails() ;
         foreach ($allusers as $oneuser) {
-            if ($oneuser->username == $this->params["create_username"]) {
+            if ($oneuser['username'] == $this->params["create_username"]) {
                 return true ; } }
         return false ;
     }
 
     private function getAllUserDetails() {
-        $signupFactory = new \Model\Signup();
-        $signup = $signupFactory->getModel($this->params);
-        $au =$signup->getUsersData();
+        $userAccountFactory = new \Model\UserAccount();
+        $userAccount = $userAccountFactory->getModel($this->params);
+        $au = $userAccount->getUsersData();
         return $au;
     }
 
     private function getOneUserDetails($username) {
-        $signupFactory = new \Model\Signup();
-        $signup = $signupFactory->getModel($this->params);
-        $au =$signup->getUsersData();
-        foreach ($au as $oneuser) {
-            if ($oneuser->username == $username) {
-                return $oneuser ; } }
-        return array() ;
+        $userAccountFactory = new \Model\UserAccount();
+        $userAccount = $userAccountFactory->getModel($this->params);
+        $au = $userAccount->getUserData($username);
+        return $au ;
     }
 
     private function deleteTheUser() {
-        $signupFactory = new \Model\Signup();
-        $signup = $signupFactory->getModel($this->params);
-        $liu = $signup->getLoggedInUserData();
-        if ($liu->username == $this->params["create_username"]) {
+        $userAccountFactory = new \Model\UserAccount();
+        $userAccount = $userAccountFactory->getModel($this->params);
+        $me = $userAccount->getLoggedInUserData() ;
+        if ($me['username'] == $this->params["create_username"]) {
             $return = array(
                 "status" => false ,
                 "message" => "You cannot delete your own user" );
             return $return ; }
-        $cu = $signup->deleteUser($this->params["create_username"]);
+        $cu = $userAccount->deleteUser($this->params["create_username"]);
         if ($cu == false) {
             $return = array(
                 "status" => false ,
