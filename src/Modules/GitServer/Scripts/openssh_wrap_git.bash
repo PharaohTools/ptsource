9ce34eb234725d4111c264a491789efb0c408a64
David Amanshia <phpengine@pharaotools.com>
2017-03-11 07:34:13 +0000

    reset perms after write

--- src/Modules/GitServer/Scripts/openssh_wrap_git.bash
+++ src/Modules/GitServer/Scripts/openssh_wrap_git.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Original command: $SSH_ORIGINAL_COMMAND" >> /tmp/sshlog ;
-echo "ptgit key var: $PTGIT_KEY" >> /tmp/sshlog ;
-echo "ptgit user var: $PTGIT_USER" >> /tmp/sshlog ;
-# echo $SSH_ORIGINAL_COMMAND ;
+#echo "Original command: $SSH_ORIGINAL_COMMAND" >> /tmp/sshlog ;
+#echo "ptgit key var: $PTGIT_KEY" >> /tmp/sshlog ;
+#echo "ptgit user var: $PTGIT_USER" >> /tmp/sshlog ;
+## echo $SSH_ORIGINAL_COMMAND ;
 
 if [[ $SSH_ORIGINAL_COMMAND == git-upload-pack* ]] || [[ $SSH_ORIGINAL_COMMAND == git-receive-pack* ]] ; then
 
@@ -15,23 +15,25 @@ if [[ $SSH_ORIGINAL_COMMAND == git-upload-pack* ]] || [[ $SSH_ORIGINAL_COMMAND =
 
 	new_command=`sed "s, '/, /,g" <<<"$new_command"`
 	new_command=`sed "s,',,g" <<<"$new_command"`
-	echo "New command: $new_command " >> /tmp/sshlog ;
+	# echo "New command: $new_command " >> /tmp/sshlog ;
 
 	repo_only=`sed "s,git-upload-pack ',,g" <<<"$SSH_ORIGINAL_COMMAND"` ;
-	echo "repo only: $repo_only " >> /tmp/sshlog ;
+	# echo "repo only: $repo_only " >> /tmp/sshlog ;
 	repo_only=`sed "s,',,g" <<<"$repo_only"` ;
-	echo "repo only: $repo_only " >> /tmp/sshlog ;
+	# echo "repo only: $repo_only " >> /tmp/sshlog ;
 
-	echo `printenv` >> /tmp/sshlog ;
+	# echo `printenv` >> /tmp/sshlog ;
 	auth_script='/home/ptgit/ptsource/openssh_auth.php';
 	# TODO the below input is risky for original command
     auth_result="php $auth_script $PTGIT_USER $repo_only " ;
-    echo "AR command: $auth_result " >> /tmp/sshlog ;
+    # echo "AR command: $auth_result " >> /tmp/sshlog ;
     is_ok=`$auth_result` ;
-    echo "res is: $is_ok " >> /tmp/sshlog ;
+    # echo "res is: $is_ok " >> /tmp/sshlog ;
 	if [ "$is_ok" == "OK" ] ; then
-        echo "res is ok" >> /tmp/sshlog ;
+        # echo "res is ok" >> /tmp/sshlog ;
         bash -c "$new_command" ;
+        chmod -R 775 /opt/ptsource/repositories/$repo_only
+        chown -R ptsource:ptgit /opt/ptsource/repositories/$repo_only
 	    exit 0 ;
 	fi
     exit 1 ;
