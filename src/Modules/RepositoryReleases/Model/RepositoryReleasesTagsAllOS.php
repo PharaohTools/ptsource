3fcdd51677b81a8a63778b3d5af466507376bad4
David Amanshia <phpengine@pharaotools.com>
2017-03-16 21:06:26 +0000

    tags model complete

--- src/Modules/RepositoryReleases/Model/RepositoryReleasesTagsAllOS.php
+++ src/Modules/RepositoryReleases/Model/RepositoryReleasesTagsAllOS.php
@@ -2,7 +2,7 @@
 
 Namespace Model;
 
-class RepositoryReleasesAllOS extends Base {
+class RepositoryReleasesTagsAllOS extends Base {
 
     // Compatibility
     public $os = array("any") ;
@@ -12,134 +12,103 @@ class RepositoryReleasesAllOS extends Base {
     public $architectures = array("any") ;
 
     // Model Group
-    public $modelGroup = array("Default") ;
-
-    public function getData() {
-        $ret = $this->getReleaseHistory();
-        $ret["repository"] = $this->getRepository();
-        $ret["tags"] = $this->getAvailableTags();
-        $ret['release_packages'] = $this->getReleasePackages();
-        $ret['enabled_default_release_packages'] =  $this->getEnabledDefaultReleasePackages($ret["repository"]);
-        $ret['standard_release_enabled'] =  $this->standardReleaseEnabled($ret["repository"]);
-        $ret['pharaoh_build_release_enabled'] =  $this->pharaohBuildReleaseEnabled($ret["repository"]);
-        return $ret ;
-    }
-
-    public function getSettingTypes() {
-        return array_keys($this->getSettingFormFields());
-    }
-
-    public function getSettingFormFields() {
-        $ff = array(
-            "stdrel_enabled" =>
-                array(
-                    "type" => "boolean",
-                    "optional" => true,
-                    "name" => "Enable Publishing Standard Releases?"
-                ),
-            "stdrel_zip_enabled" =>
-                array(
-                    "type" => "boolean",
-                    "optional" => true,
-                    "name" => "Enable Publishing Zip Archive?"
-                ),
-            "stdrel_tar_enabled" =>
-                array(
-                    "type" => "boolean",
-                    "optional" => true,
-                    "name" => "Enable Publishing Tar Archive?"
-                ),
-        );
-        return $ff ;
-    }
-
-    public function getRepository() {
-        $repositoryFactory = new \Model\Repository() ;
-        $repository = $repositoryFactory->getModel($this->params);
-        $r = $repository->getRepository($this->params["item"]);
-        return $r ;
-    }
-
-    public function getReleaseHistory() {
-        $repositoryFactory = new \Model\Repository() ;
-        $repository = $repositoryFactory->getModel($this->params, "RepositoryCommits");
-        return $repository->getCommits();
-    }
+    public $modelGroup = array("Tags") ;
 
     public function getAvailableTags() {
-        $filebrowserDir = $this->repoRootDir() ;
-        $command = "cd {$filebrowserDir}/refs/tags && ls -1" ;
+        $rrf = new \Model\RepositoryReleases() ;
+        $rr = $rrf->getModel($this->params, 'Default') ;
+        $filebrowserDir = $rr->repoRootDir() ;
+        $command = "cd {$filebrowserDir} && git show --tags" ;
         $all_tags_string = $this->executeAndLoad($command) ;
-        $all_tags_ray = explode("\n", $all_tags_string) ;
-        $all_tags_ray = array_diff($all_tags_ray, array("")) ;
+        $all_tags_ray = $this->transformTagStringToArray($all_tags_string) ;
+        $all_tags_ray = array_reverse($all_tags_ray) ;
         return $all_tags_ray ;
     }
 
-    public function getReleasePackages() {
-        $r = $this->getPharaohBuildIntegrationPackages() ;
-        $r = array_merge($r, $this->getDefaultReleasePackages()) ;
-        return $r ;
+    public function getTagCount() {
+        $all_tags_ray = $this->getAvailableTags() ;
+        $count = count($all_tags_ray) ;
+        return $count;
     }
 
-    protected function getPharaohBuildIntegrationPackages() {
-        $r = array(
-            'pharaoh_build_integration' => array(
-                'windows' => array('url' => 'http://google.com', 'title' => 'Windows Installer', 'image' => 'http://source.pharaoh.tld/Assets/Modules/RepositoryReleases/images/windows-logo.png'),
-                'linux' => array('url' => 'http://google.com', 'title' => 'Linux Installer', 'image' => 'http://source.pharaoh.tld/Assets/Modules/RepositoryReleases/images/linux-logo.png'),
-                'osx' => array('url' => 'http://google.com', 'title' => 'OSx Installer', 'image' => 'http://source.pharaoh.tld/Assets/Modules/RepositoryReleases/images/apple-logo.png'),
-            ),
-        ) ;
-        return $r ;
-    }
-
-    protected function getDefaultReleasePackages() {
-        $r = array(
-            'default' => array(
-                'git' => array('url' => 'http://google.com', 'title' => 'Git Clone', 'image' => 'http://source.pharaoh.tld/Assets/Modules/DefaultSkin/image/source-logo.png'),
-                'zip' => array('url' => 'http://google.com', 'title' => 'Zip Archive', 'image' => 'http://source.pharaoh.tld/Assets/Modules/DefaultSkin/image/source-logo.png'),
-                'tar' => array('url' => 'http://google.com', 'title' => 'Tar Archive', 'image' => 'http://source.pharaoh.tld/Assets/Modules/DefaultSkin/image/source-logo.png'),
-            ),
-        ) ;
-        return $r ;
-    }
-
-    protected function standardReleaseEnabled($repo) {
-        $mn = $this->getModuleName() ;
-        if ($repo['settings'][$mn]['stdrel_enabled']) {
-            return true ;
-        }
-        return false ;
-    }
-
-    protected function pharaohBuildReleaseEnabled($repo) {
-        $mn = $this->getModuleName() ;
-        if ($repo['settings'][$mn]['pharaoh_build_rel_enabled']) {
-            return true ;
-        }
-        return false ;
-    }
-
-    public function getEnabledDefaultReleasePackages($repo) {
-        $mn = $this->getModuleName() ;
-        if (!isset($repo['settings'][$mn])) {
-            return array() ;
-        }
-        $r = array() ;
-        if ($repo['settings'][$mn]['stdrel_enabled']) {
-            $r[] = 'git' ;
-            if ($repo['settings'][$mn]['stdrel_zip_enabled']) {
-                $r[] = 'zip' ;
-            }
-            if ($repo['settings'][$mn]['stdrel_zip_enabled']) {
-                $r[] = 'tar' ;
+    protected function transformTagStringToArray($all_tags_string) {
+        $all_lines_ray = explode("\n", $all_tags_string) ;
+        $all_lines_ray = array_diff($all_lines_ray, array("")) ;
+        $all_tags = array() ;
+        while (count($all_lines_ray)>0) {
+            if (count($all_lines_ray)>0) {
+                $result_ray = $this->loadNextArray($all_lines_ray) ;
+                $all_lines_ray = $result_ray[0] ;
+                $all_tags[] = $result_ray[1] ;
+            } else {
+                break ;
             }
         }
-        return $r ;
+        return $all_tags ;
     }
 
-    private function repoRootDir() {
-        return REPODIR.DS.$this->params["item"].DS;
-    }
 
+    protected function loadNextArray($remaining_lines) {
+
+        if (strpos($remaining_lines[0], 'tag ')===0) {
+            $tag_ray['tag'] = substr($remaining_lines[0], 4) ;
+        }
+
+        if (strpos($remaining_lines[1], 'Tagger: ')===0) {
+            $tag_ray['tagger'] = substr($remaining_lines[1], 8) ;
+        }
+
+        if (strpos($remaining_lines[2], 'Date:   ')===0) {
+            $tag_ray['date'] = substr($remaining_lines[2], 8) ;
+        }
+
+        if (strpos($remaining_lines[4], 'tag ')===0) {
+            $remaining = array_slice($remaining_lines, 4 ) ;
+//            var_dump("This remains xx", $remaining) ;
+            return array($remaining, $tag_ray) ;
+        }
+
+        if (isset($remaining_lines[4]) && $remaining_lines[4] !== '') {
+            $tag_ray['message'] = $remaining_lines[4] ;
+        }
+
+//        $tag_ray['message'] = $remaining_lines[4] ;
+
+//        var_dump("x1", $remaining_lines[5]) ;
+//        var_dump("x2", $remaining_lines[6]) ;
+//        var_dump("x3", $remaining_lines[7]) ;
+        $cur_line = 5 ;
+//        for ($i=5; $i<250; $i++) {
+//            $cur_line = $i ;
+//            if (!isset($remaining_lines[$i]) || ( isset($remaining_lines[$i]) && $remaining_lines[$i] === "\n") ) {
+//                break ;
+//            } else {
+//                var_dump('abxx:' ,$remaining_lines[$i] ) ;
+//                $tag_ray['message'] .= $remaining_lines[$i] ;
+//            }
+//        }
+//
+        $cur_line ++ ;
+
+        if (strpos($remaining_lines[$cur_line], 'tag ')===0) {
+            $remaining = array_slice($remaining_lines, $cur_line+1 ) ;
+//            var_dump("This remains 1", $remaining) ;
+            return array($remaining, $tag_ray) ;
+        }
+
+
+        if (strpos($remaining_lines[$cur_line], 'commit ')===0) {
+            $remaining = array_slice($remaining_lines, $cur_line+5 ) ;
+//            var_dump("This remains 2", $remaining) ;
+            $tag_ray['commit'] = substr($remaining_lines[$cur_line], 8) ;
+            return array($remaining, $tag_ray) ;
+        }
+
+
+//        var_dump("This remains 3", $remaining_lines[$cur_line]) ;
+
+        return array(array(), $tag_ray) ;
+
+    }
 
 }
