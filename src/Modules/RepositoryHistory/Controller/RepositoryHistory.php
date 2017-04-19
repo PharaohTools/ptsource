b816a9a0b4dc4112e7f5edca8ab4d02ad5a41603
Pharaoh Tools <phpengine@pharaohtools.com>
2017-04-19 22:12:07 +0100

    repo history has acl

--- src/Modules/RepositoryHistory/Controller/RepositoryHistory.php
+++ src/Modules/RepositoryHistory/Controller/RepositoryHistory.php
@@ -5,12 +5,18 @@ Namespace Controller ;
 class RepositoryHistory extends Base {
 
     public function execute($pageVars) {
-      $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
-      // if we don't have an object, its an array of errors
-      $this->content = $pageVars ;
-      if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
-      $this->content["data"] = $thisModel->getData();
-      return array ("type"=>"view", "view"=>"repositoryHistory", "pageVars"=>$this->content);
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        $this->content = $pageVars ;
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+
+        $repoModel = $this->getModelAndCheckDependencies('Repository', $pageVars) ;
+        if ($repoModel->userIsAllowedAccess() !== true) {
+            $override = \Core\AutoLoader::getController("Signup")  ;
+            return $override->execute() ; }
+
+        $this->content["data"] = $thisModel->getData();
+        return array ("type"=>"view", "view"=>"repositoryHistory", "pageVars"=>$this->content);
     }
 
 }
