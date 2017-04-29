84efc90197e0a73422c14fcfac8a99fc2b15fd07
Pharaoh Tools <phpengine@pharaohtools.com>
2017-04-29 11:01:52 +0100

    more about content

--- src/Modules/About/Views/AboutHTMLView.tpl.php
+++ src/Modules/About/Views/AboutHTMLView.tpl.php
@@ -1,29 +1,25 @@
 <div class="container" id="wrapper">
-    <div class="navbar-default col-sm-2 sidebar" role="navigation">
-        <div class="sidebar-nav ">
-            <ul class="nav in" id="side-menu">
-                <li class="sidebar-search">
-                    <div class="input-group custom-search-form hvr-bounce-in">
-                        <input type="text" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
+    <div class="col-lg-12">
+        <div class="well well-lg">
+            <div id="page_sidebar" class="navbar-default col-sm-2 sidebar" role="navigation">
+                <div class="sidebar-nav ">
+                    <div class="sidebar-search">
+                        <button class="btn btn-success" id="menu_visibility_label" type="button">
+                            Show Menu
+                        </button>
+                        <i class="fa fa-1x fa-toggle-off hvr-grow" id="menu_visibility_switch"></i>
                     </div>
-                    <!-- /input-group -->
-                </li>
-                <li>
-                    <a href="/index.php?control=Index&action=show" class="active hvr-bounce-in"><i class="fa fa-dashboard fa-fw hvr-grow-shadow"></i> Dashboard</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-
-    <div class="col-lg-9">
-                    <div class="well well-lg">
+                    <ul class="nav in" id="side-menu">
+                        <li>
+                            <a href="/index.php?control=Index&action=show" class="active hvr-bounce-in">
+                                <i class="fa fa-dashboard fa-fw hvr-grow-shadow"></i> Dashboard
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <div class="row clearfix no-margin">
-                <h3><a class="lg-anchor text-light" href=""> PTSource - Holding Your Code, Pharaoh Tools
+                <h3><a class="lg-anchor text-light" href=""> Pharaoh Source - Source Code Management
                         <i style="font-size: 18px;" class="fa fa-chevron-right"></i>
                     </a></h3>
 
@@ -43,12 +39,9 @@
             <div class="row clearfix no-margin">
                 <hr />
                 <p class="text-center">
-                                Visit <a href="http://www.pharaohtools.com">www.pharaohtools.com</a> for more
-
+                    Visit <a href="http://www.pharaohtools.com">www.pharaohtools.com</a> for more
             </p>
             </div>
-
         </div>
-
     </div>
 </div>
