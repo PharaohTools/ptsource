a50167d40fee4fe43a6e2dc0b779d403f7b24f73
Pharaoh Tools <phpengine@pharaohtools.com>
2017-05-10 17:38:45 +0100

    api to create repo works

--- src/Modules/RepositoryConfigure/Model/RepositoryConfigureAPIAllOS.php
+++ src/Modules/RepositoryConfigure/Model/RepositoryConfigureAPIAllOS.php
@@ -2,7 +2,7 @@
 
 Namespace Model;
 
-class PublishStatusAPIAllOS extends Base {
+class RepositoryConfigureAPIAllOS extends Base {
 
     // Compatibility
     public $os = array("any") ;
@@ -15,76 +15,33 @@ class PublishStatusAPIAllOS extends Base {
     public $modelGroup = array("API") ;
 
     public function allowedFunctions() {
-        return array('get_status') ;
+        return array('create_repository') ;
     }
 
-    public function get_status() {
-
+    public function create_repository() {
+//        ob_start();
+//        var_dump('Runnin repo configure API: ' , $this->params) ;
+//        $out = ob_get_clean() ;
+//        file_put_contents('/tmp/pharaohlog', "$out\n" . "\n\n\n", FILE_APPEND) ;
         // load repo
         $this->params['item'] = $this->params['slug'] ;
-        $pbsf = new \Model\PublishStatus() ;
-        $pbs = $pbsf->getModel($this->params) ;
-        $pipeline = $pbs->getPipeline() ;
-
-        $matching_run_id = false ;
-        $criteria = $this->calculateCriteria($this->params['criteria']) ;
-        foreach ($pipeline['history_index'] as $run_id => $historical_build) {
-
-            $matched_crits = array() ;
-            foreach ($criteria as $criterion) {
-                if ($criterion["type"] === "param") {
-                    if ($pipeline['history_index'][$run_id]['params'][$criterion["key"]] == $criterion["value"]) {
-                        $matched_crits[] = $criterion ;
-                    } else {
-                        continue 1 ;
-                    }
-                } else if ($criterion["type"] === "meta") {
-                    if ($pipeline['history_index'][$run_id]['meta'][$criterion["key"]] == $criterion["value"]) {
-                        $matched_crits[] = $criterion ;
-                    } else {
-                        continue 1 ;
-                    }
-                }
-            }
-            if (count($matched_crits) == count($criteria)) {
-                $matching_run_id = $run_id ;
-                break 1 ;
-            }
-        }
-
-        if ($matching_run_id === false) {
-            return false ;
-        }
-
-        $status_string = $pipeline['history_index'][intval($matching_run_id)]['status'] ;
-        $run_id = $matching_run_id ;
-        $run_time = $pipeline['history_index'][intval($matching_run_id)]['start'] ;
-
-        $ret = array(
-            'status' => $status_string,
-            'run_id' => $run_id,
-            'build_job_title' => $pipeline['project-name'],
-            'build_job_link' => 'http://'.$_SERVER["SERVER_NAME"].'/index.php?control=BuildHome&action=show&item='.$this->params['slug'],
-            'build_run_link' => 'http://'.$_SERVER["SERVER_NAME"].'/index.php?control=PipeRunner&action=summary&item='.$this->params['slug'].'&run-id='.$run_id,
-            'build_run_time' => $run_time,
-        ) ;
-
-        return $ret ;
-
-    }
-
-    public function calculateCriteria($criteria) {
-        $criteriaLines = explode("\n", $criteria) ;
-        $all_criteria = array() ;
-        foreach ($criteriaLines as $criteriaLine) {
-            $criteriaParts = explode(":::", $criteriaLine) ;
-            $one_criteria["type"] = $criteriaParts[0] ;
-            $one_criteria["key"] = $criteriaParts[1] ;
-            $criteriaParts[2] = rtrim($criteriaParts[2], "\n\r\t");
-            $one_criteria["value"] = $criteriaParts[2] ;
-            $all_criteria[] = $one_criteria ;
-        }
-        return $all_criteria ;
+        $params["project-name"] = $this->params["repo_name"] ;
+        $params["project-description"] = $this->params["repo_name"] ;
+        $params["project-slug"] = $this->params["repo_name"] ;
+        $params["item"] = $this->params["repo_name"] ;
+        $params["creation"] = "yes" ;
+        $rpcf = new \Model\RepositoryConfigure() ;
+        $rpc = $rpcf->getModel($params) ;
+//        ob_start();
+//        var_dump('Midway repo configure: ' ,$rpc) ;
+//        $out = ob_get_clean() ;
+//        file_put_contents('/tmp/pharaohlog', "$out\n" . "\n\n\n", FILE_APPEND) ;
+        $res = $rpc->saveRepository() ;
+//        ob_start();
+//        var_dump('Finish repo configure: ' ,$res) ;
+//        $out = ob_get_clean() ;
+//        file_put_contents('/tmp/pharaohlog', "$out\n" . "\n\n\n", FILE_APPEND) ;
+        return $res ;
     }
 
 }
