1c7b52d45f3dafe6f1a37d9fa76b9378eb7f1b70
David Amanshia <phpengine@pharaohtools.com>
2018-03-19 01:19:33 +0000

    manual upload to forced version works

--- src/Modules/ManualUpload/Assets/js/dropzone-options.js
+++ src/Modules/ManualUpload/Assets/js/dropzone-options.js
@@ -1,6 +1,14 @@
 Dropzone.options.manualUploadDrop = {
     init: function () {
         // Set up any event handlers
+        this.on("processing", function() {
+            item = $('#itemid').val() ;
+            version = document.getElementById('version').value ;
+            dz_url = "/index.php?control=ManualUpload&action=fileupload&item="+item+"&version="+version+"&output-format=SERVICE" ;
+            console.log('dz');
+            console.log(dz_url);
+            this.options.url = dz_url;
+        });
         this.on('complete', function () {
             if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
                 // reloadFiles();̣
@@ -11,6 +19,35 @@ Dropzone.options.manualUploadDrop = {
         });
     }
 };
+
+function setDZOptions(that) {
+    // console.log('that') ;
+    // console.log(that) ;
+    // console.log('Change Dropzone') ;
+    // console.log(dz_url) ;
+    // mud = $('#manualUploadDrop') ;
+    // mud.hide() ;
+    // Dropzone.options.manualUploadDrop = {
+    //     url: dz_url,
+    //     init: function () {
+    //
+    //         this.on("processing", function() {
+    //             this.options.url = dz_url;
+    //             item = $('#itemid').val() ;
+    //             version = $('#version').val() ;
+    //             dz_url = "/index.php?control=ManualUpload&action=fileupload&item="+item+"&version="+version+"&output-format=SERVICE" ;
+    //         });
+    //         this.on('complete', function () {
+    //             if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
+    //                 disableVersion();
+    //                 addDeleteLink();
+    //                 window.del_dropzone = this ;
+    //             }
+    //         });
+    //     }
+    // };
+    // mud.show() ;
+}
 
 function disableVersion() {
     $('#version').attr('disabled', 'disabled');
--- src/Modules/ManualUpload/Assets/js/manual_upload.js
+++ src/Modules/ManualUpload/Assets/js/manual_upload.js
@@ -1,4 +1,3 @@
-
 function deleteFile() {
     item = $('#itemid').val() ;
     version = $('#version').val() ;
@@ -12,18 +11,29 @@ function deleteFile() {
             window.delfilesht = data ;
             // alert('success') ;
             window.del_dropzone.removeAllFiles() ;
-
-            $('#version').attr('enabled', 'enabled');
+            ht = $($.parseJSON(window.delfilesht)) ;
+            console.log('ajax result');
+            console.log(ht);
+            result = ht[0] ;
+            if (result['status'] === 'SUCCESS') {
+                btnclass = 'btn-success' ;
+                $('#version').removeAttr('disabled');
+                $('#ajaxMessages').html('<span class="btn '+btnclass+'">'+result["outmsg"]+'</span>');
+                $('#updatable').html('');
+            } else if (result['status'] === 'FAIL'){
+                btnclass = 'btn-danger' ;
+                $('#ajaxMessages').html('<span class="btn '+btnclass+'">'+result["outmsg"]+'</span>');
+            } else if (result['status'] === 'FAIL'){
+                btnclass = 'btn-warning' ;
+                $('#ajaxMessages').html('<span class="btn '+btnclass+'">Unknown Result</span>');
+            }
         },
         failure: function(data) {
             // alert('fail') ;
+            $('#ajaxMessages').html('<span class="btn btn-danger">Error attempting to delete file</span>');
         }
     });
-    ht = $($.parseJSON(window.delfilesht)) ;
-    console.log(ht[0]["outmsg"]);
-    $('#ajaxMessages').html('<p>'+ht[0]["outmsg"]+'</p>');
 
-    dlh.hide() ;
-    ht = '' ;
-    $('#updatable').html(ht);
 }
+
+setDZOptions() ;
