b53ae6062f4d2fa95a065aff9d378de73c91ad4d
David Amanshia <phpengine@pharaohtools.com>
2018-08-03 09:03:54 +0100

    specify subdomain for build

--- vars/default.php
+++ vars/default.php
@@ -7,6 +7,7 @@ $variables['domain'] = 'pharaohtools.vm' ;
 $variables['full_slug'] = 'pt'.$variables['application_slug'] ;
 $variables['description'] = 'Pharaoh '.ucfirst($variables['application_slug']).' Development VM' ;
 $variables['subdomain'] = $variables['application_slug'] ;
+$variables['buildsubdomain'] = 'build' ;
 $variables['webclientsubdomain'] = $variables['application_slug'] ;
 $variables['server_subdomain'] = 'server' ;
 $variables['friendly_app_slug'] = $variables['application_slug'] ;
